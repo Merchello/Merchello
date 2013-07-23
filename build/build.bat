1e724ad8cbd1da05bc0fb782df22bc3ff93a19c3
Rusty Swayne <rusty@mindfly.com>
2013-07-22 22:36:18 -0700

    Patching up some configuration issues resulting from .gitignore changes.  Also tweaked the target Platform - should be revisited.

--- build/build.bat
+++ build/build.bat
@@ -2,10 +2,11 @@
 SET release=1.0.0.0
 SET comment=
 SET version=%release%
+SET package=true
 
 IF [%comment%] EQU [] (SET version=%release%) ELSE (SET version=%release%-%comment%)
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Merchello.build" /target:Build /p:BUILD_RELEASE=%release% /p:BUILD_COMMENT=%comment%
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Merchello.build" /target:Build /p:BUILD_RELEASE=%release% /p:BUILD_COMMENT=%comment% /p:PACKAGE_ARCHIVES=%package%
 
 IF ERRORLEVEL 1 GOTO :showerror
 
