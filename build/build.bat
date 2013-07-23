adc83445e9fe7fcddf826d149248560b3c83386e
Rusty Swayne <rusty@mindfly.com>
2013-07-22 17:09:34 -0700

    Updated build scripts to zip binaries for artifact download.  Also, setup initial NuGet package.

--- build/build.bat
+++ build/build.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
-SET release=1.0.0
+SET release=1.0.0.0
 SET comment=
 SET version=%release%
 
 IF [%comment%] EQU [] (SET version=%release%) ELSE (SET version=%release%-%comment%)
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Merchello.build" /p:BUILD_RELEASE=%release% /p:BUILD_COMMENT=%comment%
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Merchello.build" /target:Build /p:BUILD_RELEASE=%release% /p:BUILD_COMMENT=%comment%
 
 IF ERRORLEVEL 1 GOTO :showerror
 
