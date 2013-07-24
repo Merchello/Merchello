7920c193585f58cd397d5f7e5f9b66744748c030
Rusty Swayne <rusty@mindfly.com>
2013-07-23 23:41:00 -0700

    Cleaned up build.bat

--- src/Merchello.Core/Properties/AssemblyInfo.cs
+++ src/Merchello.Core/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.63478")]
+[assembly: AssemblyFileVersion("1.0.0.64629")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
