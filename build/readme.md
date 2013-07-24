9f77400964837249cc5830a8b027a04c4329b4fe
Rusty Swayne <rusty@mindfly.com>
2013-07-23 17:49:51 -0700

    Refactored build script.  Still working on the Umbraco Package Portion.

---
+++ build/readme.md
@@ -0,0 +1,3 @@
+Merchello
+
+A new high performance, open source eCommerce package for Umbraco being built with store owners and designers in mind.
--- test/Merchello.Core.Tests/BuildServerSetupTests/BuildSetupTestFixture.cs
+++ test/Merchello.Core.Tests/BuildServerSetupTests/BuildSetupTestFixture.cs
@@ -12,11 +12,12 @@ namespace Merchello.Core.Tests.BuildServerSetupTests
     public class BuildSetupTestFixture
     {
         [Test]
-        public void Assembly_Version_Correct()
+        public void CurrentVersion_Equals_AssemblyVersion()
         {
+            var current = MerchelloVersion.Current.ToString();
             var version = MerchelloVersion.AssemblyVersion;
             
-            StringAssert.AreEqualIgnoringCase("1.0.0.0", version);
+            StringAssert.AreEqualIgnoringCase(current, version);
         }
 
     }
