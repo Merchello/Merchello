8bf4db009e2d7086b03fd0b39a6a0909e804e7b9
Rusty Swayne <rusty@mindfly.com>
2013-07-24 07:37:48 -0700

    Added NUnit testing.

--- src/Merchello.Core/Properties/AssemblyInfo.cs
+++ src/Merchello.Core/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.64629")]
+[assembly: AssemblyFileVersion("1.0.1.18501")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
