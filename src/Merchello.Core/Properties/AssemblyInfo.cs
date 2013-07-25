216c27e98dba7451d5e22eda67df0b80d29642a4
ablemods_jpayne <jpayne@ablemods.com>
2013-07-25 18:28:27 -0400

    testing build script in local environment

--- src/Merchello.Core/Properties/AssemblyInfo.cs
+++ src/Merchello.Core/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.1.18501")]
+[assembly: AssemblyFileVersion("1.0.2.32656")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
