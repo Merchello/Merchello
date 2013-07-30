b49ebbbe4137bfe4bfef3ee50c96a84ddb4d556e
Rusty Swayne <rusty@mindfly.com>
2013-07-30 10:59:55 -0700

    Initial work on the MerchelloSection configuration section. Added constructs to map database types (look up values) to be more extendable than simply having an integer value representing a look up type.  Also addresses the issue of enumerations mapped to specific integer values.

---
+++ src/Merchello.Core/Configuration/Outline/TypeFieldDefinitionsElement.cs
@@ -0,0 +1,56 @@
+﻿using System.Configuration;
+
+
+namespace Merchello.Core.Configuration.Outline
+{
+    public class TypeFieldDefinitionsElement : ConfigurationElement
+    {
+
+        /// <summary>
+        /// Gets the dbTypeFields configuration collection for customer address
+        /// </summary>
+        [ConfigurationProperty("customerAddress", IsRequired = false), ConfigurationCollection(typeof(TypeFieldCollection), AddItemName = "type")]
+        public TypeFieldCollection CustomerAddress
+        {
+            get { return (TypeFieldCollection)this["customerAddress"]; }
+        }
+
+        /// <summary>
+        /// Gets the dbTypeFields configuration collection for baskets
+        /// </summary>
+        [ConfigurationProperty("basket", IsRequired = true), ConfigurationCollection(typeof(TypeFieldCollection), AddItemName = "type")]
+        public TypeFieldCollection Basket
+        {
+            get { return (TypeFieldCollection)this["basket"]; }
+        }
+
+
+        /// <summary>
+        /// Gets the dbTypeFields configuration collection for shipping methods
+        /// </summary>
+        [ConfigurationProperty("shipMethod", IsRequired = false), ConfigurationCollection(typeof(TypeFieldCollection), AddItemName = "type")]
+        public TypeFieldCollection ShipMethod
+        {
+            get { return (TypeFieldCollection)this["shipMethod"]; }
+        }
+
+        /// <summary>
+        /// Gets the dbTypeFields configuration collection for invoice items
+        /// </summary>
+        [ConfigurationProperty("invoiceItem", IsRequired = false), ConfigurationCollection(typeof(TypeFieldCollection), AddItemName = "type")]
+        public TypeFieldCollection InvoiceItem
+        {
+            get { return (TypeFieldCollection)this["invoiceItem"]; }
+        }
+
+        /// <summary>
+        /// Gets the dbTypeFields configuration collection for IProduct
+        /// </summary>
+        [ConfigurationProperty("product", IsRequired = false), ConfigurationCollection(typeof(TypeFieldCollection), AddItemName = "type")]
+        public TypeFieldCollection Product
+        {
+            get { return (TypeFieldCollection)this["product"]; }
+        }
+
+    }
+}
---
+++ test/Merchello.Core.Tests/Configuration_Tests/MerchelloSectionConfigurationSection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+using Merchello.Core.Configuration.Outline;
+using NUnit.Framework;
+
+namespace Merchello.Core.Tests.Configuration_Tests
+{
+    [TestFixture]
+    public class MerchelloSectionConfigurationSection
+    {
+        private MerchelloSection _config;
+        private TypeFieldCollection _invoiceItems;
+
+        [SetUp]
+        public void Setup()
+        {
+            _config = ConfigurationManager.GetSection("merchello") as MerchelloSection;
+            _invoiceItems = _config.TypeFields.InvoiceItem;
+        }
+
+        [Test]
+        public void Customer_Address_Residential_Guid_Matches()
+        {
+            var guid = new Guid("D32D7B40-2FF2-453F-9AC5-51CF1A981E46");
+            Assert.AreEqual(guid, _config.TypeFields.CustomerAddress[CustomerAddress.Residential.ToString()].TypeKey);            
+        }
+
+        [Test]
+        public void Invoice_Item_Collection_Contains_5_Elements()
+        {
+            Assert.AreEqual(5, _invoiceItems.Count);
+        }
+
+
+        [Test]
+        public void Invoice_Item_Charge_Name_Is_Charge()
+        {
+            Assert.AreEqual("Charge or Fee", _invoiceItems["Charge"].Name);
+        }
+
+        [Test]
+        public void Product_Item_Collection_Is_Empty()
+        {
+            Assert.IsEmpty(_config.TypeFields.Product);
+        }
+
+        private enum CustomerAddress
+        {
+            Residential,
+            Commercial
+        }
+
+    }
+
+
+}
