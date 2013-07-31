e9107d44142b6a95d335a094452d05dbb8c82075
Rusty Swayne <rusty@mindfly.com>
2013-07-30 22:27:20 -0700

    Initial work on the Customer model

---
+++ src/Merchello.Core/Models/ICustomer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Umbraco.Core.Models.EntityBase;
+
+namespace Merchello.Core.Models
+{
+    /// <summary>
+    /// Defines a Merchello customer
+    /// </summary>
+    public interface ICustomer : IAggregateRoot
+    {
+        /// <summary>
+        /// The Umbraco member Id
+        /// </summary>
+        [DataMember]
+        int? MemberId { get; set; }
+
+        /// <summary>
+        /// The first name of the customer
+        /// </summary>
+        [DataMember]
+        string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the customer
+        /// </summary>
+        [DataMember]
+        string LastName { get; set; }
+
+        /// <summary>
+        /// The total amount this customer has been invoiced
+        /// </summary>
+        [DataMember]
+        decimal TotalInvoiced { get; }
+
+        /// <summary>
+        /// The total amount this customer has paid
+        /// </summary>
+        [DataMember]
+        decimal TotalPayments { get; }
+
+        /// <summary>
+        /// The date the customer made their last payment
+        /// </summary>
+        [DataMember]
+        DateTime? LastPaymentDate { get; }
+    }
+}
