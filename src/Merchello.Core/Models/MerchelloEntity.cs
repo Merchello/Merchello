95f7cd3f307be3a78a6dea0cc1aa1e260c310ffd
Rusty Swayne <rusty@mindfly.com>
2013-08-01 22:16:37 -0700

    Exposes more of internal Umbraco Entity

--- src/Merchello.Core/Models/MerchelloEntity.cs
+++ src/Merchello.Core/Models/MerchelloEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.Serialization;
 using Umbraco.Core.Models.EntityBase;
 
 namespace Merchello.Core.Models
@@ -10,8 +11,36 @@ namespace Merchello.Core.Models
     /// </summary>
     public abstract class MerchelloEntity : Entity
     {
+        
+        internal virtual void ResetIdentity()
+        {
+            HasIdentity = false;
+            Id = default(int);
+        }
 
+        internal virtual void MarkHasIdentity()
+        {
+            HasIdentity = true;
+        }
 
+        /// <summary>
+        /// Method to call on entity saved when first added
+        /// </summary>
+        internal virtual void AddingEntity()
+        {
+            CreateDate = DateTime.Now;
+            UpdateDate = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Method to call on entity saved/updated
+        /// </summary>
+        internal virtual void UpdatingEntity()
+        {
+            UpdateDate = DateTime.Now;
+        }
+
+        
 
         /// <summary>
         /// Used by inheritors to set the value of properties, this will detect if the property value actually changed and if it did
