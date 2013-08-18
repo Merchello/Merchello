9a20abce5dd8da45f866fe9b01c26ecc50d56777
Rusty Swayne <rusty@mindfly.com>
2013-08-18 13:47:57 -0700

    Refactored service tests to setup from a base class for use in other services tests

--- test/Merchello.Tests.Base/Respositories/MockUnitOfWorkProvider.cs
+++ test/Merchello.Tests.Base/Respositories/MockUnitOfWorkProvider.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Merchello.Tests.Base.Respositories.UnitOfWork;
+﻿using Merchello.Tests.Base.Respositories.UnitOfWork;
 using Umbraco.Core.Persistence.UnitOfWork;
 
-namespace Merchello.Tests.UnitTests.Repository
+namespace Merchello.Tests.Base.Respositories
 {
     public class MockUnitOfWorkProvider : IDatabaseUnitOfWorkProvider
     {
