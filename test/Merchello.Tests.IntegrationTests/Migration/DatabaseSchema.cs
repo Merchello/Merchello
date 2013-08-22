b7271ec7c63b1b885cca119d0109112539fb5abb
Rusty Swayne <rusty@mindfly.com>
2013-08-22 12:09:08 -0700

    Added SqlCe database for automated Integration testing.

--- test/Merchello.Tests.IntegrationTests/Migration/DatabaseSchema.cs
+++ test/Merchello.Tests.IntegrationTests/Migration/DatabaseSchema.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using Merchello.Core.Configuration.Outline;
 using Merchello.Core.Persistence.Migrations.Initial;
+using Merchello.Tests.Base.Db;
+using Merchello.Tests.Base.SqlSyntax;
 using NUnit.Framework;
 using Umbraco.Core.Persistence;
 using Umbraco.Core.Persistence.SqlSyntax;
@@ -8,32 +11,20 @@ using Umbraco.Core.Persistence.SqlSyntax;
 namespace Merchello.Tests.IntegrationTests.Migration
 {
     [TestFixture]
-    public class DatabaseSchema  
+    public class DatabaseSchema  : DbIntegrationTestBase
     {
-        private Database _database;
-
-        [SetUp]
-        public void Setup()
-        {
-            var config = (MerchelloSection) ConfigurationManager.GetSection("merchello");
-            var connectionString = ConfigurationManager.ConnectionStrings[config.DefaultConnectionStringName].ConnectionString;
-
-            _database = new Database(connectionString, "System.Data.SqlClient");
-
-            SqlSyntaxContext.SqlSyntaxProvider = new SqlServerSyntaxProvider();
-        }
-
+       
         [Test]
         public void Can_Drop_All_Database_Tables()
         {
-            var deletions = new DatabaseSchemaCreation(_database);
+            var deletions = new DatabaseSchemaCreation(Database);
             deletions.UninstallDatabaseSchema();
         }
 
         [Test]
         public void Successfully_Create_Default_Database_Schema()
         {
-            var creation = new DatabaseSchemaCreation(_database);
+            var creation = new DatabaseSchemaCreation(Database);
             creation.InitializeDatabaseSchema();
         }
 
