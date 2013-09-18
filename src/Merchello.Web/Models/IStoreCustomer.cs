c8cd5960eb3c6bc284ca589f3b725c6aacf9444c
Rusty Swayne <rusty@mindfly.com>
2013-09-18 15:34:52 -0700

    Prototype for business object to represent a customer.  Need a lot of discussion around these!

---
+++ src/Merchello.Web/Models/IStoreCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Merchello.Core.Models;
+
+namespace Merchello.Web.Models
+{
+    public interface IStoreCustomer
+    {
+        ICustomer Customer { get; }
+
+        IEnumerable<IInvoice> Invoices { get; }
+
+        IEnumerable<IPayment> Payments { get; }
+
+        IEnumerable<IAddress> Addresses { get; }
+    }
+}
