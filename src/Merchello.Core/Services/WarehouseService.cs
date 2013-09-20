a1f7e017d897152889b2c5a2e78e24889feff803
Rusty Swayne <rusty@mindfly.com>
2013-09-20 13:03:19 -0700

    Initial warehouse service

---
+++ src/Merchello.Core/Services/WarehouseService.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Merchello.Core.Models;
+using Merchello.Core.Persistence;
+using Merchello.Core.Events;
+using Umbraco.Core;
+using Umbraco.Core.Persistence.UnitOfWork;
+
+namespace Merchello.Core.Services
+{
+    /// <summary>
+    /// Represents the Warehouse Service 
+    /// </summary>
+    public class WarehouseService : IWarehouseService
+    {
+        private readonly IDatabaseUnitOfWorkProvider _uowProvider;
+        private readonly RepositoryFactory _repositoryFactory;
+
+        private static readonly ReaderWriterLockSlim Locker = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+
+        public WarehouseService()
+            : this(new RepositoryFactory())
+        { }
+
+        public WarehouseService(RepositoryFactory repositoryFactory)
+            : this(new PetaPocoUnitOfWorkProvider(), repositoryFactory)
+        { }
+
+        public WarehouseService(IDatabaseUnitOfWorkProvider provider, RepositoryFactory repositoryFactory)
+        {
+            Mandate.ParameterNotNull(provider, "provider");
+            Mandate.ParameterNotNull(repositoryFactory, "repositoryFactory");
+
+            _uowProvider = provider;
+            _repositoryFactory = repositoryFactory;
+        }
+
+        #region IWarehouseService Members
+
+
+        /// <summary>
+        /// Creates an <see cref="IWarehouse"/> object
+        /// </summary>
+        public IWarehouse CreateWarehouse(string name)
+        {
+            Mandate.ParameterNotNull(name, "name");
+
+            var warehouse = new Warehouse()
+                {
+                    Name = name
+                };
+                
+            Created.RaiseEvent(new NewEventArgs<IWarehouse>(warehouse), this);
+
+            return warehouse;
+        }
+
+        /// <summary>
+        /// Saves a single <see cref="IWarehouse"/> object
+        /// </summary>
+        /// <param name="warehouse">The <see cref="IWarehouse"/> to save</param>
+        /// <param name="raiseEvents">Optional boolean indicating whether or not to raise events.</param>
+        public void Save(IWarehouse warehouse, bool raiseEvents = true)
+        {
+            if (raiseEvents) Saving.RaiseEvent(new SaveEventArgs<IWarehouse>(warehouse), this);
+
+            using (new WriteLock(Locker))
+            {
+                var uow = _uowProvider.GetUnitOfWork();
+                using (var repository = _repositoryFactory.CreateWarehouseRepository(uow))
+                {
+                    repository.AddOrUpdate(warehouse);
+                    uow.Commit();
+                }
+
+                if (raiseEvents) Saved.RaiseEvent(new SaveEventArgs<IWarehouse>(warehouse), this);
+            }
+        }
+
+        /// <summary>
+        /// Saves a collection of <see cref="IWarehouse"/> objects.
+        /// </summary>
+        /// <param name="warehouseList">Collection of <see cref="Warehouse"/> to save</param>
+        /// <param name="raiseEvents">Optional boolean indicating whether or not to raise events</param>
+        public void Save(IEnumerable<IWarehouse> warehouseList, bool raiseEvents = true)
+        {
+            var warehouseArray = warehouseList as IWarehouse[] ?? warehouseList.ToArray();
+
+            if (raiseEvents) Saving.RaiseEvent(new SaveEventArgs<IWarehouse>(warehouseArray), this);
+
+            using (new WriteLock(Locker))
+            {
+                var uow = _uowProvider.GetUnitOfWork();
+                using (var repository = _repositoryFactory.CreateWarehouseRepository(uow))
+                {
+                    foreach (var warehouse in warehouseArray)
+                    {
+                        repository.AddOrUpdate(warehouse);
+                    }
+                    uow.Commit();
+                }
+            }
+
+            if (raiseEvents) Saved.RaiseEvent(new SaveEventArgs<IWarehouse>(warehouseArray), this);
+        }
+
+        /// <summary>
+        /// Deletes a single <see cref="IWarehouse"/> object
+        /// </summary>
+        /// <param name="warehouse">The <see cref="IWarehouse"/> to delete</param>
+        /// <param name="raiseEvents">Optional boolean indicating whether or not to raise events</param>
+        public void Delete(IWarehouse warehouse, bool raiseEvents = true)
+        {
+            if (raiseEvents) Deleting.RaiseEvent(new DeleteEventArgs<IWarehouse>( warehouse), this);
+
+            using (new WriteLock(Locker))
+            {
+                var uow = _uowProvider.GetUnitOfWork();
+                using (var repository = _repositoryFactory.CreateWarehouseRepository(uow))
+                {
+                    repository.Delete( warehouse);
+                    uow.Commit();
+                }
+            }
+            if (raiseEvents) Deleted.RaiseEvent(new DeleteEventArgs<IWarehouse>( warehouse), this);
+        }
+
+        /// <summary>
+        /// Deletes a collection <see cref="IWarehouse"/> objects
+        /// </summary>
+        /// <param name="warehouseList">Collection of <see cref="IWarehouse"/> to delete</param>
+        /// <param name="raiseEvents">Optional boolean indicating whether or not to raise events</param>
+        public void Delete(IEnumerable<IWarehouse> warehouseList, bool raiseEvents = true)
+        {
+            var warehouseArray = warehouseList as IWarehouse[] ?? warehouseList.ToArray();
+
+            if (raiseEvents) Deleting.RaiseEvent(new DeleteEventArgs<IWarehouse>(warehouseArray), this);
+
+            using (new WriteLock(Locker))
+            {
+                var uow = _uowProvider.GetUnitOfWork();
+                using (var repository = _repositoryFactory.CreateWarehouseRepository(uow))
+                {
+                    foreach (var warehouse in warehouseArray)
+                    {
+                        repository.Delete(warehouse);
+                    }
+                    uow.Commit();
+                }
+            }
+
+            if (raiseEvents) Deleted.RaiseEvent(new DeleteEventArgs<IWarehouse>(warehouseArray), this);
+        }
+
+        /// <summary>
+        /// Gets a Warehouse by its unique id - pk
+        /// </summary>
+        /// <param name="id">int Id for the Warehouse</param>
+        /// <returns><see cref="IWarehouse"/></returns>
+        public IWarehouse GetById(int id)
+        {
+            using (var repository = _repositoryFactory.CreateWarehouseRepository(_uowProvider.GetUnitOfWork()))
+            {
+                return repository.Get(id);
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of Warehouse give a list of unique keys
+        /// </summary>
+        /// <param name="ids">List of unique keys</param>
+        /// <returns></returns>
+        public IEnumerable<IWarehouse> GetByIds(IEnumerable<int> ids)
+        {
+            using (var repository = _repositoryFactory.CreateWarehouseRepository(_uowProvider.GetUnitOfWork()))
+            {
+                return repository.GetAll(ids.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of warehouses associated with a ship method
+        /// </summary>
+        /// <param name="shipMethodId">The id of the <see cref="IShipMethod"/></param>
+        /// <returns>A collection of <see cref="IWarehouse"/></returns>
+        public IEnumerable<IWarehouse> GetWarehousesForShipMethod(int shipMethodId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public IEnumerable<IWarehouse> GetAll()
+        {
+            using (var repository = _repositoryFactory.CreateWarehouseRepository(_uowProvider.GetUnitOfWork()))
+            {
+                return repository.GetAll();
+            }
+        }
+
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Occurs before Delete
+        /// </summary>		
+        public static event TypedEventHandler<IWarehouseService, DeleteEventArgs<IWarehouse>> Deleting;
+
+        /// <summary>
+        /// Occurs after Delete
+        /// </summary>
+        public static event TypedEventHandler<IWarehouseService, DeleteEventArgs<IWarehouse>> Deleted;
+
+        /// <summary>
+        /// Occurs before Save
+        /// </summary>
+        public static event TypedEventHandler<IWarehouseService, SaveEventArgs<IWarehouse>> Saving;
+
+        /// <summary>
+        /// Occurs after Save
+        /// </summary>
+        public static event TypedEventHandler<IWarehouseService, SaveEventArgs<IWarehouse>> Saved;
+
+        /// <summary>
+        /// Occurs before Create
+        /// </summary>
+        public static event TypedEventHandler<IWarehouseService, NewEventArgs<IWarehouse>> Creating;
+
+        /// <summary>
+        /// Occurs after Create
+        /// </summary>
+        public static event TypedEventHandler<IWarehouseService, NewEventArgs<IWarehouse>> Created;
+
+        #endregion
+
+
+     
+    }
+}
---
+++ test/Merchello.Tests.Base/DataMakers/AddressMock.cs
@@ -0,0 +1,45 @@
+﻿using Merchello.Core;
+using Merchello.Core.Models;
+using Merchello.Core.Models.TypeFields;
+
+namespace Merchello.Tests.Base.DataMakers
+{
+    internal class AddressMock
+    {
+        public string Name { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Locality { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string CountryCode { get; set; }
+
+        public IAddress MakeAddress(ICustomer customer, string label)
+        {
+            return new Address(customer.Key, label)
+            {
+                FullName = string.Format("{0} {1}", customer.FirstName, customer.LastName).Trim(),
+                Address1 = this.Address1,
+                Address2 = this.Address2,
+                Locality = this.Locality,
+                Region = this.Region,
+                PostalCode = this.PostalCode,
+                CountryCode = this.CountryCode,
+                AddressTypeFieldKey = EnumTypeFieldConverter.Address().GetTypeField(AddressType.Residential).TypeKey
+            };
+        }
+
+        public IWarehouse MakeWarehouse()
+        {
+            return new Warehouse()
+            {
+                Name = Name,
+                Address1 = Address1,
+                Address2 = Address2,
+                Locality = Locality,
+                Region = Region,
+                PostalCode = PostalCode
+            };
+        }
+    }
+}
