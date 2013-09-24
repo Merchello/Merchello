880d238ec7973c3a1fa45bb7a34683901659e7f2
Jason Prothero <jason@proworks.com>
2013-09-24 16:24:17 -0700

    Just checking in EOD progress

---
+++ src/Merchello.Web.UI/Trees/MerchelloActionNewProduct.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using umbraco.interfaces;
+using umbraco.BasePages;
+
+namespace Merchello.Web.UI.Trees
+{
+    public class MerchelloActionNewProduct : IAction
+	{
+		//create singleton
+		private static readonly MerchelloActionNewProduct InnerInstance = new MerchelloActionNewProduct();
+
+
+        private MerchelloActionNewProduct() { }
+
+        public static MerchelloActionNewProduct Instance
+		{
+			get { return InnerInstance; }
+		}
+
+		#region IAction Members
+
+		public char Letter
+		{
+			get
+			{
+				return 'C';
+			}
+		}
+
+		public string JsFunctionName
+		{
+			get
+			{
+				return null;
+			}
+		}
+
+		public string JsSource
+		{
+			get
+			{
+				return null;
+			}
+		}
+
+		public string Alias
+		{
+			get
+			{
+				return "CreateProduct";
+			}
+		}
+
+		public string Icon
+		{
+			get
+			{
+                return "plus";
+			}
+		}
+
+		public bool ShowInNotifier
+		{
+			get
+			{
+				return true;
+			}
+		}
+		public bool CanBePermissionAssigned
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		#endregion
+	}
+}
