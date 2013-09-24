b235379e91340ce803a5afdc26e292d42169817f
koswesley <wesley@proworks.com>
2013-09-24 15:39:36 -0700

    Customers WebApi, Customers Unit Tests, Fix for Moq DLL issue

--- src/Merchello.Web/Editors/CustomerApiController.cs
+++ src/Merchello.Web/Editors/CustomerApiController.cs
@@ -20,6 +20,8 @@ namespace Merchello.Web.Editors
     [PluginController("Merchello")]
     public class CustomerApiController : MerchelloApiController
     {
+		private ICustomerService _customerService;
+
         /// <summary>
         /// Constructor
         /// </summary>
@@ -51,13 +53,95 @@ namespace Merchello.Web.Editors
         /// <param name="key"></param>
         public Customer GetCustomer(Guid key)
         {
-            var customer = MerchelloContext.Services.CustomerService.GetByKey(key) as Customer;
-            if (customer == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
+			if (key != null)
+			{
+				var customer = MerchelloContext.Services.CustomerService.GetByKey(key) as Customer;
+				if (customer == null)
+				{
+					throw new HttpResponseException(HttpStatusCode.NotFound);
+				}
 
-            return customer;
+				return customer;
+			}
+			else
+			{
+				var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
+				{
+					Content = new StringContent(String.Format("Parameter key is null")),
+					ReasonPhrase = "Invalid Parameter"
+				};
+				throw new HttpResponseException(resp);
+			}
         }
+
+		/// <summary>
+		///  Creates a customer from FirstName, LastName, Email and MemberId
+		///  
+		/// GET /umbraco/Merchello/CustomerApi/NewCustomer?firstName=FIRSTNAME&lastName=LASTNAME&email=EMAIL&memberId=0
+		/// </summary>
+		/// <param name="firstName">Customers First Name</param>
+		/// <param name="lastName">Customers Last Name</param>
+		/// <param name="email">Customers Email Address</param>
+		/// <param name="memberId">Optional: MemberId</param>
+		/// <returns>New Customer</returns>		
+		[AcceptVerbs("GET", "POST")]
+		public Customer NewCustomer(string firstName, string lastName, string email, int? memberId = null)
+		{
+			Customer newCustomer = null;
+			
+			try
+			{
+				newCustomer = _customerService.CreateCustomer(firstName, lastName, email, memberId) as Customer;
+			}
+			catch
+			{		   
+				throw new HttpResponseException(HttpStatusCode.InternalServerError);
+			}
+			return newCustomer;
+		}
+		/// <summary>
+		/// Updates an existing Customer
+		/// 
+		/// PUT /umbraco/Merchello/CustomerApi/PutCustomer
+		/// </summary>
+		/// <param name="customer">Customer To Update</param>
+		/// <returns>Http Response</returns>
+		public HttpResponseMessage PutCustomer(Customer customer)
+		{
+			var response = Request.CreateResponse(HttpStatusCode.OK);
+
+			try
+			{
+				_customerService.Save(customer);
+			}
+			catch (Exception ex)
+			{
+				response = Request.CreateResponse(HttpStatusCode.InternalServerError, String.Format("{0}", ex.Message));
+			}
+			return response;  	
+		}
+
+		/// <summary>
+		/// Deletes an existing customer
+		/// 
+		/// DELETE /umbraco/Merchello/CustomerApi/{guid}
+		/// </summary>
+		/// <param name="key">Guid of Customer</param>
+		/// <returns>Http Response</returns>
+		public HttpResponseMessage Delete(Guid key)
+		{
+			var customerToDelete = _customerService.GetByKey(key);
+			var response = Request.CreateResponse(HttpStatusCode.OK);
+
+			if (customerToDelete == null)
+			{
+				response = Request.CreateResponse(HttpStatusCode.NotFound);
+				return response;
+			}
+
+			_customerService.Delete(customerToDelete);
+
+			return response;
+		}
     }
 }
