d43a6be700029b9c0312af14b0e27b2e7283ab66
Rusty Swayne <rusty@mindfly.com>
2013-10-06 17:04:05 -0700

    ProductVariant

---
+++ src/Merchello.Core/Persistence/Repositories/ProductVariantRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Merchello.Core.Models;
+using Merchello.Core.Persistence.Caching;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.Querying;
+using Umbraco.Core.Persistence.UnitOfWork;
+
+namespace Merchello.Core.Persistence.Repositories
+{
+    internal class ProductVariantRepository : MerchelloPetaPocoRepositoryBase<Guid, IProductVariant>, IProductVariantRepository
+    {
+        public ProductVariantRepository(IDatabaseUnitOfWork work) 
+            : base(work)
+        { }
+
+        public ProductVariantRepository(IDatabaseUnitOfWork work, IRepositoryCacheProvider cache) 
+            : base(work, cache)
+        {
+        }
+
+        #region Overrides MerchelloPetaPocoRepositoryBase
+
+        protected override IProductVariant PerformGet(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<IProductVariant> PerformGetAll(params Guid[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<IProductVariant> PerformGetByQuery(IQuery<IProductVariant> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Sql GetBaseQuery(bool isCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetBaseWhereClause()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDeleteClauses()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PersistNewItem(IProductVariant entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PersistUpdatedItem(IProductVariant entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+    }
+}
