133dd6572ff97eea1794fb7bc07a13e0659f4a20
Rusty Swayne <rusty@mindfly.com>
2013-10-10 23:31:38 -0700

    Touch ups on the MerchelloApiController abstract class

--- src/Merchello.Web/WebApi/MerchelloApiController.cs
+++ src/Merchello.Web/WebApi/MerchelloApiController.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
+using System.Web.Http.Controllers;
 using Merchello.Core;
-using Merchello.Core.Services;
 using Newtonsoft.Json.Serialization;
 using Umbraco.Web;
 using Umbraco.Web.WebApi;
@@ -40,7 +35,7 @@ namespace Merchello.Web.WebApi
         /// Removes the xml formatter and configure the camel casing
         /// </summary>
         /// <param name="controllerContext"></param>
-        protected override void Initialize(global::System.Web.Http.Controllers.HttpControllerContext controllerContext)
+        protected override void Initialize(HttpControllerContext controllerContext)
         {
             base.Initialize(controllerContext);
 
