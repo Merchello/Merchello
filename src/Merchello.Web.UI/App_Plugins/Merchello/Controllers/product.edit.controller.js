9b556bc70922e7ded826d00d12370977c3f79a61
Jason Prothero <jason@proworks.com>
2013-10-18 21:18:38 -0700

    Start of the variant generation code

--- src/Merchello.Web.UI/App_Plugins/Merchello/Controllers/product.edit.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Controllers/product.edit.controller.js
@@ -6,7 +6,7 @@
  * @description
  * The controller for the product editor
  */
-function ProductEditController($scope, $routeParams, $location, notificationsService, angularHelper, serverValidationManager, merchelloProductService) {
+function ProductEditController($scope, $routeParams, $location, notificationsService, dialogService, angularHelper, serverValidationManager, merchelloProductService) {
 
     if ($routeParams.create) {
         $scope.loaded = true;
@@ -22,6 +22,10 @@ function ProductEditController($scope, $routeParams, $location, notificationsSer
         promise.then(function (product) {
 
             $scope.product = product;
+            if (product.productOptions.length > 0)
+            {
+                $scope.product.hasoptions = true;
+            }
             $scope.loaded = true;
             $scope.preValuesLoaded = true;
             $(".content-column-body").css('background-image', 'none');
@@ -51,13 +55,75 @@ function ProductEditController($scope, $routeParams, $location, notificationsSer
         });
     };
 
+    $scope.chooseMedia = function () {
+
+        dialogService.mediaPicker({
+            multipicker: true,
+            callback: function (data) {
+                _.each(data.selection, function (media, i) {
+                    //shortcuts
+                    //TODO, do something better then this for searching
+
+                    var img = {};
+                    img.id = media.id;
+                    img.src = imageHelper.getImagePropertyValue({ imageModel: media });
+                    img.thumbnail = imageHelper.getThumbnailFromPath(img.src);
+                    $scope.images.push(img);
+                    $scope.ids.push(img.id);
+                });
+
+                $scope.sync();
+            }
+        });
+
+    };
+
     $scope.addOption = function () {
-        $scope.product.productOptions.push({ name: "", required: 0, sortOrder: $scope.product.productOptions.length + 1, choices: [{name: "one", sku: "one-sku", sortOrder: 1}] });
+        $scope.product.productOptions.push({ name: "", required: 0, sortOrder: $scope.product.productOptions.length + 1, choices: [{ name: "one", sku: "one-sku", sortOrder: 1 }] });
         createVariants();
     };
 
     createVariants = function () {
-        
+
+        var choicesIndex = new Array();
+        var choicesLength = new Array();
+
+        $scope.product.productVariants = [];
+
+        // Init indexes and lengths
+        for (var o = 0; o < $scope.product.productOptions.length; o++)
+        {
+            choicesIndex.push(0);
+            choicesLength.push($scope.product.productOptions[o].choices.length);
+        }
+
+        while (choicesIndex[0] < choicesLength[0])
+        {
+            // get current choices from indexes and create variants if they don't exist
+            var variant = { productAttributes:[] }
+            for (c = 0; c < choicesIndex.length; c++)
+            {
+                variant.productAttributes.push($scope.product.productOptions[c].choices[choicesIndex[c]]);
+            }
+
+            $scope.product.productVariants.push(variant);
+
+            // loop through indexes from the last one to the first one (check if the last exists and increment the next one up)
+            for (var i = choicesIndex.length-1; i >= 0; i--) {
+
+                choicesIndex[i] = choicesIndex[i] + 1;
+                
+                if (choicesIndex[i] <= choicesLength[i])
+                {
+                    break;
+                }
+                else
+                {
+                    choicesIndex[i] = 0;
+                }
+            }
+
+        }
     };
 }
 
