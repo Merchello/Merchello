ba9537298463c390be3b9d75eadc354d21e9c548
Jason Prothero <jason@proworks.com>
2013-10-24 12:31:28 -0700

    Product models updated, product edit and variant edit updates, product list sorting table headers and limits

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Catalog/product.edit.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Catalog/product.edit.controller.js
@@ -8,7 +8,7 @@
      * @description
      * The controller for the product editor
      */
-    controllers.ProductEditController = function($scope, $routeParams, $location, notificationsService, dialogService, angularHelper, serverValidationManager, merchelloProductService) {
+    controllers.ProductEditController = function ($scope, $routeParams, $location, notificationsService, dialogService, angularHelper, serverValidationManager, merchelloProductService, merchelloProductVariantService) {
 
         if ($routeParams.create) {
             $scope.loaded = true;
@@ -23,11 +23,8 @@
 
             promise.then(function (product) {
 
-                $scope.product = product;
-                if (product.productOptions.length > 0)
-                {
-                    $scope.product.hasoptions = true;
-                }
+                $scope.product = new merchello.Models.Product(product);
+
                 $scope.loaded = true;
                 $scope.preValuesLoaded = true;
                 $(".content-column-body").css('background-image', 'none');
@@ -80,53 +77,6 @@
 
         };
 
-        $scope.addOption = function () {
-            $scope.product.productOptions.push({ name: "", required: 0, sortOrder: $scope.product.productOptions.length + 1, choices: [{ name: "one", sku: "one-sku", sortOrder: 1 }] });
-            createVariants();
-        };
-
-        createVariants = function () {
-
-            var choicesIndex = new Array();
-            var choicesLength = new Array();
-
-            $scope.product.productVariants = [];
-
-            // Init indexes and lengths
-            for (var o = 0; o < $scope.product.productOptions.length; o++)
-            {
-                choicesIndex.push(0);
-                choicesLength.push($scope.product.productOptions[o].choices.length);
-            }
-
-            while (choicesIndex[0] < choicesLength[0])
-            {
-                // get current choices from indexes and create variants if they don't exist
-                var variant = { productAttributes:[] }
-                for (c = 0; c < choicesIndex.length; c++)
-                {
-                    variant.productAttributes.push($scope.product.productOptions[c].choices[choicesIndex[c]]);
-                }
-
-                $scope.product.productVariants.push(variant);
-
-                // loop through indexes from the last one to the first one (check if the last exists and increment the next one up)
-                for (var i = choicesIndex.length-1; i >= 0; i--) {
-
-                    choicesIndex[i] = choicesIndex[i] + 1;
-                
-                    if (choicesIndex[i] <= choicesLength[i])
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        choicesIndex[i] = 0;
-                    }
-                }
-
-            }
-        };
     }
 
     angular.module("umbraco").controller("Merchello.Editors.Product.EditController", merchello.Controllers.ProductEditController);
