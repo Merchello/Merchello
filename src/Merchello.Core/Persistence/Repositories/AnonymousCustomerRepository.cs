4eefb3c44a58509e02705f3b94902a7fdd5ac4e8
Rusty Swayne <rusty@mindfly.com>
2013-11-04 10:07:37 -0800

    Removes memberId from AnonymousCustomerDto and patches ItemCache cascade delete issue on AnonymousCustomer deletions.

--- src/Merchello.Core/Persistence/Repositories/AnonymousCustomerRepository.cs
+++ src/Merchello.Core/Persistence/Repositories/AnonymousCustomerRepository.cs
@@ -90,7 +90,7 @@ namespace Merchello.Core.Persistence.Repositories
         {
             var list = new List<string>
                 {
-                    "DELETE FROM merchItemCacheItem WHERE itemCacheId = (SELECT id FROM merchItemCache WHERE entityKey = @Id)",
+                    "DELETE FROM merchItemCacheItem WHERE itemCacheId IN (SELECT id FROM merchItemCache WHERE entityKey = @Id)",
                     "DELETE FROM merchItemCache WHERE entityKey = @Id",
                     "DELETE FROM merchAnonymousCustomer WHERE pk = @Id",
                 };
