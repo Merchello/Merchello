833ee1acc9c14d7f8714522e6c01de9d9d3e0961
Rusty Swayne <rusty@mindfly.com>
2013-11-13 14:17:19 -0800

    Removed references to Examine test project.

--- src/Merchello.Examine/Properties/AssemblyVisibility.cs
+++ src/Merchello.Examine/Properties/AssemblyVisibility.cs
@@ -3,4 +3,3 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.Base")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.UnitTests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.IntegrationTests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.IntegrationTests.Examine")]
--- src/Merchello.Web/Properties/AssemblyVisibility.cs
+++ src/Merchello.Web/Properties/AssemblyVisibility.cs
@@ -1,4 +1,3 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.Base")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.UnitTests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.IntegrationTests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.IntegrationTests.Examine")]
