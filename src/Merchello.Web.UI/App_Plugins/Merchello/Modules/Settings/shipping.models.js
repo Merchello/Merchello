0a68c000739b5163941badf85e5b52d6a2ab00be
Kyle Weems <kyle@mindfly.com>
2013-11-19 14:22:29 -0800

    Created shipping.models.js, started working on models for shipping.

---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/shipping.models.js
@@ -0,0 +1,62 @@
+﻿(function (models, undefined) {
+
+    models.shippingCountry = function (shippingCountryFromServer) {
+        
+        var self = this;
+
+        if (shippingRegionFromServer == undefined) {
+            self.key = "";
+            self.name = "";
+            self.shippingMethods = [];
+        } else {
+            self.key = shippingCountryFromServer.key;
+            self.name = shippingCountryFromServer.name;
+            self.shippingMethods = _.map(shippingCountryFromServer, function (attribute) {
+                return new merchello.Models.shippingMethod(attribute);
+            });
+        }
+
+    };
+
+    models.ShippingMethod = function (shippingMethodFromServer) {
+
+        var self = this;
+
+        if (shippingMethodFromServer == undefined) {
+            self.key = "";
+            self.name = "";
+            self.warehousesSupported = [];
+            var 
+        } else {
+
+        }
+
+    };
+
+    models.Warehouse = function (warehouseFromServer) {
+
+        var self = this;
+
+        if (warehouseFromServer == undefined) {
+            self.key = "";
+            self.name = "";
+            self.address = {
+                streetAddress: "",
+                streetAddress2: "",
+                locality: "",
+                region: "",
+                postalCode: "",
+                country: ""
+            };
+            self.isPrimary = true;
+        } else {
+            self.key = warehouseFromServer.key;
+            self.name = warehouseFromServer.name;
+            self.address = warehouseFromServer.address;
+            self.isPrimary = warehouseFromServer.isPrimary;
+        }
+
+    };
+
+}(window.merchello.Models = window.merchello.Models || {}));
+
