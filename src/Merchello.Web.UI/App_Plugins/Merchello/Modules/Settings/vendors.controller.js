b942230dc2c69418ffef302e61ede53f9a62f591
Kyle Weems <kyle@mindfly.com>
2013-11-21 15:23:34 -0800

    Wired in the Vendors model to the Vendors controller, created ng-repeat bind for listing.

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/vendors.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/vendors.controller.js
@@ -10,8 +10,50 @@
      */
     controllers.VendorsController = function ($scope, $routeParams, $location, notificationsService, angularHelper, serverValidationManager, merchelloProductService) {
 
-        $scope.loaded = true;
-        $scope.preValuesLoaded = true;
+        $scope.vendors = [];
+        $scope.sortProperty = "name";
+        $scope.sortOrder = "asc";
+
+        $scope.loadVendors = function () {
+
+            // just a mock of getting the vendor objects
+            var mockVendors = [
+                {
+                    name: "Geeky Soap",
+                    contact: "Anne-Marie",
+                    phone: "360-647-7470",
+                    address1: "114 W Magnolia St",
+                    address2: "Suite 504",
+                    locality: "Bellingham",
+                    region: "WA",
+                    postalCode: "98225",
+                    country: "United States of America"
+                },
+                {
+                    name: "Nerdy Soap",
+                    contact: "Heather",
+                    phone: "360-647-7470",
+                    address1: "105 W Holly St H22",
+                    address2: "",
+                    locality: "Bellingham",
+                    region: "WA",
+                    postalCode: "98225",
+                    country: "United States of America"
+                }
+            ];
+            $scope.vendors = _.map(mockVendors, function (vendorFromServer) {
+                return new merchello.Models.Vendor(vendorFromServer);
+            });
+            // end of mocks
+
+
+            $scope.loaded = true;
+            $scope.preValuesLoaded = true;
+
+        };
+
+        $scope.loadVendors();
+
 
     }
 
