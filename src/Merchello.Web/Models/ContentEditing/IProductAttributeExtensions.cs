327d7674cacf5e7f12ba5172e93c0c7f04dd4797
Jason Prothero <jason@proworks.com>
2013-11-20 17:10:33 -0800

    directives, ProductEdit, extensions, etc

---
+++ src/Merchello.Web/Models/ContentEditing/IProductAttributeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Merchello.Core;
+using Merchello.Core.Models;
+
+namespace Merchello.Web.Models.ContentEditing
+{
+    public static class IProductAttributeExtensions
+    {
+        public static ProductAttributeDisplay ToProductDisplay(this IProductAttribute productAttribute)
+        {
+            AutoMapper.Mapper.CreateMap<IProductAttribute, ProductAttributeDisplay>();
+
+            return AutoMapper.Mapper.Map<ProductAttributeDisplay>(productAttribute);
+        }
+    }
+}
---
+++ src/Merchello.Web/Models/ContentEditing/IProductVariantExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Merchello.Core;
+using Merchello.Core.Models;
+
+namespace Merchello.Web.Models.ContentEditing
+{
+    public static class IProductVariantExtensions
+    {
+        public static ProductVariantDisplay ToProductDisplay(this IProductVariant productVariant)
+        {
+            AutoMapper.Mapper.CreateMap<IProductAttribute, ProductAttributeDisplay>();
+            AutoMapper.Mapper.CreateMap<IProductVariant, ProductVariantDisplay>();
+
+            return AutoMapper.Mapper.Map<ProductVariantDisplay>(productVariant);
+        }
+    }
+}
