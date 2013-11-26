463704e5ce7ca759449a1e47c2f9915150ea77b0
Kyle Weems <kyle@mindfly.com>
2013-11-26 14:31:35 -0800

    Added model, controller, and wired up view for manual payment methods in Settings/Payment.

---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/payment.models.js
@@ -0,0 +1,19 @@
+﻿(function (models, undefined) {
+
+    models.ManualPaymentMethod = function (manualPaymentMethodFromServer) {
+
+        var self = this;
+
+        if (manualPaymentMethodFromServer == undefined) {
+            self.pk = "";
+            self.name = "";
+            self.description = "";
+        } else {
+            self.pk = manualPaymentMethodFromServer.pk;
+            self.name = manualPaymentMethodFromServer.name;
+            self.description = manualPaymentMethodFromServer.description;
+        };
+
+    };
+
+}(window.merchello.Models = window.merchello.Models || {}));
