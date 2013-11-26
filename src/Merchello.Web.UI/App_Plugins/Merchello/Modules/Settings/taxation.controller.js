0255ba0490976c97a014d65e73cc3926cd53fa3b
Kyle Weems <kyle@mindfly.com>
2013-11-25 16:52:40 -0800

    Added model, mocks, and wired in AngularJS for the Setting/Taxation page.

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/taxation.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/taxation.controller.js
@@ -10,8 +10,47 @@
      */
     controllers.TaxationController = function ($scope, $routeParams, $location, notificationsService, angularHelper, serverValidationManager, merchelloProductService) {
 
-        $scope.loaded = true;
-        $scope.preValuesLoaded = true;
+        $scope.taxRates = [];
+        $scope.sortProperty = "name";
+        $scope.sortOrder = "asc";
+        $scope.deleteTaxRate = new merchello.Models.TaxRate();
+        $scope.newTaxRate = new merchello.Models.TaxRate();
+
+        $scope.loadTaxRates = function () {
+
+            // Note From Kyle: Mocks for data returned from Tax Rates API call.
+            var mockTaxRates = [
+                {
+                    pk: 0,
+                    country: "Canada",
+                    rate: 11.5
+                },
+                {
+                    pk: 1,
+                    country: "United Kingdom",
+                    rate: 4
+                },
+                {
+                    pk: 2,
+                    country: "United States",
+                    rate: 7.8
+                },
+                {
+                    pk: 3,
+                    country: "Everywhere Else",
+                    rate: 0
+                }
+            ];
+            $scope.taxRates = _.map(mockTaxRates, function (taxRateFromServer) {
+                return new merchello.Models.TaxRate(taxRateFromServer);
+            });
+            // End of Tax Rates API mocks.
+            $scope.loaded = true;
+            $scope.preValuesLoaded = true;
+
+        };
+
+        $scope.loadTaxRates();
 
     }
 
---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/taxation.models.js
@@ -0,0 +1,19 @@
+﻿(function (models, undefined) {
+
+    models.TaxRate = function (taxRateFromServer) {
+
+        var self = this;
+
+        if (taxRateFromServer == undefined) {
+            self.pk = "";
+            self.country = "";
+            self.rate = "";
+        } else {
+            self.pk = taxRateFromServer.pk;
+            self.country = taxRateFromServer.country;
+            self.rate = taxRateFromServer.rate;
+        };
+
+    };
+
+}(window.merchello.Models = window.merchello.Models || {}));
