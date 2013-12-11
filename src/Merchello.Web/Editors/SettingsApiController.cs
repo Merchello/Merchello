39a11a4082bb3dc6b6b39e954087691e3a3a5aac
Jason Prothero <jason@proworks.com>
2013-12-10 16:22:37 -0800

    controllers, display, mapping for Settings and Warehouses

---
+++ src/Merchello.Web/Editors/SettingsApiController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using Merchello.Core;
+using Merchello.Core.Models;
+using Merchello.Core.Services;
+using Merchello.Web.WebApi;
+using Merchello.Web.Models.ContentEditing;
+using System.Net;
+
+namespace Merchello.Web.Editors
+{
+    [PluginController("Merchello")]
+    public class SettingsApiController : MerchelloApiController
+    {
+        private readonly ISettingsService _settingsService;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SettingsApiController()
+            : this(MerchelloContext.Current)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="merchelloContext"></param>
+        public SettingsApiController(MerchelloContext merchelloContext)
+            : base(merchelloContext)
+        {
+            _settingsService = MerchelloContext.Services.SettingsService;
+        }
+
+        /// <summary>
+        /// This is a helper contructor for unit testing
+        /// </summary>
+        internal SettingsApiController(MerchelloContext merchelloContext, UmbracoContext umbracoContext)
+            : base(merchelloContext, umbracoContext)
+        {
+            _settingsService = MerchelloContext.Services.SettingsService;
+        }
+
+        /// <summary>
+        /// Returns Country for the countryCode passed in
+        /// 
+        /// GET /umbraco/Merchello/SettingsApi/GetCountry/{countryCode}
+        /// </summary>
+        /// <param name="id">Country code to get</param>
+        public CountryDisplay GetCountry(string id)
+        {
+            ICountry country = _settingsService.GetCountryByCode(id);
+            if (country == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return country.ToCountryDisplay();
+        }
+
+        /// <summary>
+        /// Returns All Countries
+        /// 
+        /// GET /umbraco/Merchello/SettingsApi/GetAllCountries
+        /// </summary>
+        public IEnumerable<CountryDisplay> GetAllCountries()
+        {
+            var countries = _settingsService.GetAllCountries();
+            if (countries == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            foreach (ICountry country in countries)
+            {
+                yield return country.ToCountryDisplay();
+            }
+        }
+
+        /// <summary>
+        /// Returns All Countries with a list of country codes to exclude
+        /// 
+        /// GET /umbraco/Merchello/SettingsApi/GetAllCountriesExcludeCodes?codes={string}&codes={string}
+        /// </summary>
+        /// <param name="codes">Country codes to exclude</param>
+        public IEnumerable<CountryDisplay> GetAllCountriesExcludeCodes([FromUri]string[] codes)
+        {
+            var countries = _settingsService.GetAllCountries(codes);
+            if (countries == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            foreach (ICountry country in countries)
+            {
+                yield return country.ToCountryDisplay();
+            }
+        }
+    }
+}
---
+++ src/Merchello.Web/Models/ContentEditing/SettingsMappingExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Merchello.Core;
+using Merchello.Core.Models;
+
+namespace Merchello.Web.Models.ContentEditing
+{
+    internal static class SettingsMappingExtensions
+    {
+
+        #region CountryDisplay
+
+        internal static CountryDisplay ToCountryDisplay(this ICountry country)
+        {
+            AutoMapper.Mapper.CreateMap<ICountry, CountryDisplay>();
+            AutoMapper.Mapper.CreateMap<IProvince, ProvinceDisplay>();
+
+            return AutoMapper.Mapper.Map<CountryDisplay>(country);
+        }
+
+        #endregion
+    }
+}
