68ef472f53e42b9a7f33d9109f01dfc224e38b43
koswesley <wesley@proworks.com>
2014-01-28 16:25:26 -0800

    Settings API, and Angular. saving doesn't work yet.

---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/merchello.settings.service.js
@@ -0,0 +1,40 @@
+﻿(function (merchelloServices, undefined) {
+
+
+    /**
+        * @ngdoc service
+        * @name umbraco.resources.MerchelloProductService
+        * @description Loads in data for data types
+        **/
+    merchelloServices.MerchelloSettingsService = function ($q, $http, umbDataFormatter, umbRequestHelper, notificationsService, merchelloProductVariantService) {
+
+        var settingsservice = {     
+
+            /** saves or updates a product object */
+            save: function (settingDisplay) {
+                
+                return umbRequestHelper.resourcePromise(
+                   $http.post(
+                        umbRequestHelper.getApiUrl('merchellSettingsApiBaseUrl', 'PutSetting'),
+                        settingDisplay
+                    ),
+                    'Failed to save data for Store Settings');
+            },
+
+            getAllSettings: function () {
+
+                return umbRequestHelper.resourcePromise(
+                    $http.get(
+                        umbRequestHelper.getApiUrl('merchellSettingsApiBaseUrl', 'GetAllSettings')
+                    ),
+                    'Failed to get all Settings');     
+            },                      
+
+        };
+
+        return settingsservice;
+    };
+
+    angular.module('umbraco.resources').factory('merchelloSettingsService', merchello.Services.MerchelloSettingsService);
+
+}(window.merchello.Services = window.merchello.Services || {}));
---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/settings.models.js
@@ -0,0 +1,28 @@
+﻿(function (models, undefined) {                                                                                                          
+
+    models.SettingDisplay = function (settingsFromServer) {
+
+        var self = this;
+        if (settingsFromServer == undefined) {
+            self.currencyCode = "";
+            self.nextOrderNumber = 0;
+            self.nextInvoiceNumber = 0;
+            self.dateFormat = "";
+            self.timeFormat = "";
+            self.globalShippable = false;
+            self.globalTaxable = false;
+            self.globalTrackInventory = false;
+        }
+        else {
+            self.currencyCode = settingsFromServer.currencyCode;
+            self.nextOrderNumber = settingsFromServer.nextOrderNumber;
+            self.nextInvoiceNumber = settingsFromServer.nextInvoiceNumber;
+            self.dateFormat = settingsFromServer.dateFormat;
+            self.timeFormat = settingsFromServer.timeFormat;
+            self.globalShippable = settingsFromServer.globalShippable;
+            self.globalTaxable = settingsFromServer.globalTaxable;
+            self.globalTrackInventory = settingsFromServer.globalTrackInventory;
+        }
+    };
+
+}(window.merchello.Models = window.merchello.Models || {}));
