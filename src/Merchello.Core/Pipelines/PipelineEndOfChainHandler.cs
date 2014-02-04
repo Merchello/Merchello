ec41fd6801de583d8c9e949aa19420c3e011f768
Rusty Swayne <rusty@mindfly.com>
2014-02-03 22:44:11 -0800

    Roughing out a very simple pipeline

---
+++ src/Merchello.Core/Pipelines/PipelineEndOfChainHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Umbraco.Core;
+
+namespace Merchello.Core.Pipelines
+{
+    /// <summary>
+    /// Represents an end of chain PipelineTaskHander.  This terminates the task chain.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class PipelineEndOfChainHandler<T> : IPipelineTaskHandler<T>
+
+    {
+        private static readonly PipelineEndOfChainHandler<T> _instance = new PipelineEndOfChainHandler<T>();
+
+        public static PipelineEndOfChainHandler<T> Instance
+        {
+            get { return _instance; }
+        }
+
+        /// <summary>
+        /// Executes the task
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns><see cref="Attempt"/> of T</returns>
+        public Attempt<T> Execute(T arg)
+        {
+            return Attempt<T>.Succeed(arg);
+        }
+
+        /// <summary>
+        /// Registers the next task
+        /// </summary>
+        /// <param name="next"></param>
+        public void RegisterNext(IPipelineTaskHandler<T> next)
+        {
+            throw new InvalidOperationException("Cannot register next on the end of chain.");
+        }
+    }
+}
---
+++ test/Merchello.Tests.UnitTests/Pipeline/MockPipelineTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Merchello.Core.Pipelines;
+using NUnit.Framework;
+using Umbraco.Core;
+
+namespace Merchello.Tests.UnitTests.Pipeline
+{
+    [TestFixture]
+    public class MockPipelineTests
+    {
+        private IEnumerable<TestPipelineTaskHandler> _tasks;
+        private int _taskCount = 10;
+
+        [SetUp]
+        public void Init()
+        {
+            var taskList = new List<TestPipelineTaskHandler>();
+            //for (int i = 0; i < _taskCount; i++)
+            //{
+            //    var newTask = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = i }); 
+            //    taskList.Add(newTask);
+            //    if (i >= 1) taskList[i - i].RegisterNext(newTask);
+            //}
+
+            var task1 = new TestPipelineTaskHandler(new DemoPipelineTask() {Index = 1});
+            var task2 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 2 });
+            var task3 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 3 });
+            var task4 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 4 });
+            var task5 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 5 });
+            var task6 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 6 });
+            var task7 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 7 });
+            var task8 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 8 });
+            var task9 = new TestPipelineTaskHandler(new DemoPipelineTask() { Index = 9 });
+
+            task1.RegisterNext(task2);
+            task2.RegisterNext(task3);
+            task3.RegisterNext(task4);
+            task4.RegisterNext(task5);
+            task5.RegisterNext(task6);
+            task6.RegisterNext(task7);
+            task7.RegisterNext(task8);
+            task8.RegisterNext(task9);
+
+            taskList.Add(task1);
+            taskList.Add(task2);
+            taskList.Add(task3);
+            taskList.Add(task4);
+            taskList.Add(task5);
+            taskList.Add(task6);
+            taskList.Add(task7);
+            taskList.Add(task8);
+            taskList.Add(task9);
+
+            _tasks = taskList;
+
+        }
+
+        [Test]
+        public void Can_Execute_All_Tasks_In_The_Pipeline()
+        {
+            //// Arrange
+            var first = _tasks.FirstOrDefault();
+            Assert.NotNull(first);
+
+            //// Act
+            var attempt = first.Execute(0);
+
+            //// Assert
+            Assert.IsTrue(attempt.Success);
+            Console.Write(attempt.Result);
+        }
+    }
+
+    internal class TestPipelineTaskHandler : PipelineTaskHandlerBase<int>
+    {
+        public TestPipelineTaskHandler(IPipelineTask<int> task) : base(task)
+        {
+        }
+    }
+
+    internal class DemoPipelineTask : IPipelineTask<int>
+    {
+        public int Index { get; set; }
+
+        public Attempt<int> PerformTask(int arg)
+        {
+            var addOne = arg + 1;
+            return Attempt.Succeed(addOne);
+        }
+    }
+}
