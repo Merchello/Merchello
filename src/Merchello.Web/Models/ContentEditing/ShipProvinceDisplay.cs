bd108313b4fd67d3a77269bca6a6ff4e9e67e5e9
Rusty Swayne <rusty@mindfly.com>
2014-02-06 19:14:06 -0800

    Housekeeping

--- src/Merchello.Web/Models/ContentEditing/ShipProvinceDisplay.cs
+++ src/Merchello.Web/Models/ContentEditing/ShipProvinceDisplay.cs
@@ -5,8 +5,6 @@ namespace Merchello.Web.Models.ContentEditing
 {
     public class ShipProvinceDisplay : ProvinceDisplay
     {
-        //public string Name { get; set; }
-        //public string Code { get; set; }
         public bool AllowShipping { get; set; }
         public decimal RateAdjustment { get; set; }
         public RateAdjustmentType RateAdjustmentType { get; set; }
