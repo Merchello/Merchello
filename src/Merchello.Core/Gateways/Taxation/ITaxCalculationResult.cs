6aa10b45b801d1c367c9a98c466fe9d088bed782
Rusty Swayne <rusty@mindfly.com>
2014-02-24 12:55:50 -0800

    Missed files

---
+++ src/Merchello.Core/Gateways/Taxation/ITaxCalculationResult.cs
@@ -0,0 +1,31 @@
+﻿using Merchello.Core.Models;
+
+namespace Merchello.Core.Gateways.Taxation
+{
+    /// <summary>
+    /// Defines an invoice tax calculation result
+    /// </summary>
+    public interface ITaxCalculationResult
+    {
+        /// <summary>
+        /// The name of the Tax Method
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The tax used in the tax calculation
+        /// </summary>
+        decimal TaxRate { get; }
+
+        /// <summary>
+        /// The calculated tax amount
+        /// </summary>
+        decimal TaxAmount { get; set; }
+
+        /// <summary>
+        /// An extended data collection to store any additional information returned by the Tax Provider.
+        /// Ex. may include an itemization of Country, State, Local / Municipal taxes
+        /// </summary>
+        ExtendedDataCollection ExtendedData { get; }
+    }
+}
---
+++ src/Merchello.Core/Gateways/Taxation/TaxCalculationResult.cs
@@ -0,0 +1,33 @@
+﻿using Merchello.Core.Models;
+
+namespace Merchello.Core.Gateways.Taxation
+{
+    /// <summary>
+    /// Represents an invoice tax calculation result
+    /// </summary>
+    public class TaxCalculationResult : ITaxCalculationResult
+    {
+        public TaxCalculationResult(decimal taxRate, decimal taxAmount)
+            : this(string.Empty, taxRate, taxAmount)
+        {}
+
+        public TaxCalculationResult(string name, decimal taxRate, decimal taxAmount)
+            : this(name, taxRate, taxAmount, new ExtendedDataCollection())
+        { }
+
+        public TaxCalculationResult(string name, decimal taxRate, decimal taxAmount, ExtendedDataCollection extendedData)
+        {
+            Mandate.ParameterNotNull(extendedData, "extendedData");
+
+            Name = string.IsNullOrEmpty(name) ? "Tax" : name;
+            TaxRate = taxRate;
+            TaxAmount = taxAmount;
+            ExtendedData = extendedData;
+        }
+
+        public string Name { get; private set; }
+        public decimal TaxRate { get; private set; }
+        public decimal TaxAmount { get; set; }
+        public ExtendedDataCollection ExtendedData { get; private set; }
+    }
+}
