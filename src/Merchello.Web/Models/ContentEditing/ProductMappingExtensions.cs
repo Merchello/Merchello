545da25dfbad24a33ae6c980137b9ca8d6000b52
Rusty Swayne <rusty@mindfly.com>
2014-03-06 14:31:11 -0800

    #M-161 Fixed

--- src/Merchello.Web/Models/ContentEditing/ProductMappingExtensions.cs
+++ src/Merchello.Web/Models/ContentEditing/ProductMappingExtensions.cs
@@ -52,10 +52,20 @@ namespace Merchello.Web.Models.ContentEditing
                 }
             }
 
+            // Fix option deletion here #M-161
+            // remove any product options that exist in destination and do not exist in productDisplay
+            var removers = destination.ProductOptions.Where(x => !productDisplay.ProductOptions.Select(pd => pd.Key).Contains(x.Key)).Select(x => x.Key);
+            foreach (var remove in removers)
+            {
+                destination.ProductOptions.RemoveItem(remove);
+            }
+
+
             foreach (var option in productDisplay.ProductOptions)
             {
                 IProductOption destinationProductOption;
-                // TODO RUSTY: Please help with option deletion here #M-
+                
+
                 if (destination.ProductOptions.Contains(option.Key))
                 {
                     destinationProductOption = destination.ProductOptions[option.Key];
@@ -128,6 +138,15 @@ namespace Merchello.Web.Models.ContentEditing
             }
             destinationProductOption.Required = productOptionDisplay.Required;
             destinationProductOption.SortOrder = productOptionDisplay.SortOrder;
+
+
+            // Fix with option deletion here #M-161
+            // remove any product choices that exist in destination and do not exist in productDisplay
+            var removers = destinationProductOption.Choices.Where(x => !productOptionDisplay.Choices.Select(pd => pd.Key).Contains(x.Key)).Select(x => x.Key).ToArray();
+            foreach (var remove in removers)
+            {
+                destinationProductOption.Choices.RemoveItem(remove);
+            }
 
             foreach (var choice in productOptionDisplay.Choices)
             {
