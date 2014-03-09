3ec86c0f8b51bd379ba0060da025863a99e4a83a
Jason Prothero <jason@proworks.com>
2014-03-09 14:56:32 -0700

    Made product picker in content area use a umbraco dialog.  Prepping for a cooler dialog interface.

---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/PropertyEditors/ProductPicker/Js/merchelloproductdialog.controller.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+(function (controllers, undefined) {
+
+    /**
+     * @ngdoc controller
+     * @name Merchello.PropertyDialogs.MerchelloProductDialog
+     * @function
+     * 
+     * @description
+     * The controller for the selecting products on the Umbraco dialog flyout
+     */
+    controllers.MerchelloProductDialog = function ($scope, merchelloProductService, notificationsService) {
+
+        $scope.selectedProduct = {};
+        $scope.products = [];
+
+        $scope.loadProducts = function () {
+
+            var promise = merchelloProductService.getAllProducts();
+
+            promise.then(function (products) {
+
+                $scope.products = _.map(products, function (productFromServer) {
+                    return new merchello.Models.Product(productFromServer, true);
+                });
+
+            }, function (reason) {
+
+                notificationsService.error("Products Dialog Load Failed", reason.message);
+
+            });
+
+        };
+
+        $scope.loadProducts();
+    };
+
+    angular.module("umbraco").controller("Merchello.PropertyDialogs.MerchelloProductDialog", merchello.Controllers.MerchelloProductDialog);
+
+
+}(window.merchello.Controllers = window.merchello.Controllers || {}));
--- src/Merchello.Web.UI/App_Plugins/Merchello/PropertyEditors/ProductPicker/Js/merchelloproductselector.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/PropertyEditors/ProductPicker/Js/merchelloproductselector.controller.js
@@ -2,27 +2,63 @@
 
 (function () {
 
-    function productSelector($scope, merchelloProductService) {
+    function productSelector($scope, merchelloProductService, notificationsService, dialogService, assetsService) {
 
-        $scope.loadProducts = function () {
+        // TODO: this doesn't seem to hook up the angular correctly.  Check with Per if this is possible.
+        // load the dialog controller javascript on demand, then load the product if it worked.
+        //assetsService.loadJs("/App_Plugins/Merchello/PropertyEditors/ProductPicker/Js/merchelloproductdialog.controller.js").then(
+        //    function () {
+        //        if (_.isString($scope.model.value)) {
+        //            if ($scope.model.value.length > 0) {
+        //                $scope.loadProduct($scope.model.value);
+        //            }
+        //        }
+        //    });
 
-            var promise = merchelloProductService.getAllProducts();
+        $scope.product = {};
+        $scope.loaded = false;
 
-            promise.then(function (products) {
+        $scope.loadProduct = function (key) {
 
-                $scope.products = _.map(products, function (productFromServer) {
-                    return new merchello.Models.Product(productFromServer, true);
-                });
+            var promise = merchelloProductService.getByKey(key);
+
+            promise.then(function (product) {
+
+                $scope.product = new merchello.Models.Product(product);
+
+                $scope.loaded = true;
 
             }, function (reason) {
-
-                notificationsService.error("Products Selector Load Failed", reason.message);
-
+                notificationsService.error("Product Load Failed", reason.message);
             });
 
         };
 
-        $scope.loadProducts();
+        $scope.selectedProductFromDialog = function (selectedProduct) {
+
+            $scope.model.value = selectedProduct.key;
+            $scope.product = selectedProduct;
+
+        };
+
+        $scope.selectProduct = function () {
+
+            dialogService.open({
+                template: '/App_Plugins/Merchello/PropertyEditors/ProductPicker/Views/merchelloproductdialog.html',
+                show: true,
+                callback: $scope.selectedProductFromDialog,
+                dialogData: $scope.product
+            });
+
+        };
+
+        // Load the product from the Guid key stored in the model.value
+        if (_.isString($scope.model.value)) {
+            if ($scope.model.value.length > 0) {
+                $scope.loadProduct($scope.model.value);
+            }
+        }
+
     };
 
     angular.module("umbraco").controller('Merchello.PropertyEditors.MerchelloProductSelector', productSelector);
