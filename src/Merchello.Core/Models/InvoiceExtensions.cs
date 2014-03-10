77e2f606664296a84424e8d4dcc35f25a29089ea
Rusty Swayne <rusty@mindfly.com>
2014-03-09 18:22:31 -0700

    Housekeeping

--- src/Merchello.Core/Models/InvoiceExtensions.cs
+++ src/Merchello.Core/Models/InvoiceExtensions.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using Merchello.Core.Builders;
 using Merchello.Core.Gateways.Payment;
 using Merchello.Core.Gateways.Taxation;
 using Merchello.Core.Services;
-using Umbraco.Core;
 using Umbraco.Core.Logging;
 
 namespace Merchello.Core.Models
@@ -18,40 +16,6 @@ namespace Merchello.Core.Models
     public static class InvoiceExtensions
     {
         /// <summary>
-        /// Calculates taxes for the invoice
-        /// </summary>
-        /// <param name="invoice">The <see cref="IInvoice"/></param>
-        /// <returns>The <see cref="ITaxCalculationResult"/> from the calculation</returns>
-        public static ITaxCalculationResult CalculateTaxes(this IInvoice invoice)
-        {
-            return invoice.CalculateTaxes(invoice.GetBillingAddress());
-        }
-
-        /// <summary>
-        /// Calculates taxes for the invoice
-        /// </summary>
-        /// <param name="invoice">The <see cref="IInvoice"/></param>
-        /// <param name="taxAddress">The address (generally country code and region) to be used to determine the taxation rates</param>
-        /// <returns>The <see cref="ITaxCalculationResult"/> from the calculation</returns>
-        public static ITaxCalculationResult CalculateTaxes(this IInvoice invoice, IAddress taxAddress)
-        {
-            return invoice.CalculateTaxes(MerchelloContext.Current, taxAddress);
-        }
-
-        /// <summary>
-        /// Calculates taxes for the invoice
-        /// </summary>
-        /// <param name="invoice">The <see cref="IInvoice"/></param>
-        /// <param name="merchelloContext">The <see cref="IMerchelloContext"/></param>
-        /// <param name="taxAddress">The address (generally country code and region) to be used to determine the taxation rates</param>
-        /// <returns>The <see cref="ITaxCalculationResult"/> from the calculation</returns>
-        public static ITaxCalculationResult CalculateTaxes(this IInvoice invoice, IMerchelloContext merchelloContext, IAddress taxAddress)
-        {
-            // remove any other tax lines
-            return merchelloContext.Gateways.Taxation.CalculateTaxesForInvoice(invoice, taxAddress);
-        }
-
-        /// <summary>
         /// Returns a constructed invoice number (including it's invoice number prefix - if any)
         /// </summary>
         /// <param name="invoice">The <see cref="IInvoice"/></param>
@@ -62,6 +26,8 @@ namespace Merchello.Core.Models
                 ? invoice.InvoiceNumber.ToString(CultureInfo.InvariantCulture)
                 : string.Format("{0}-{1}", invoice.InvoiceNumberPrefix, invoice.InvoiceNumber);
         }
+
+        #region Order
 
         /// <summary>
         /// Prepares an <see cref="IOrder"/> without saving it to the database.  
@@ -107,6 +73,9 @@ namespace Merchello.Core.Models
             throw attempt.Exception;
         }
 
+        #endregion
+
+        #region AppliedPayments
 
         /// <summary>
         /// Returns a collection of <see cref="IAppliedPayment"/> for the invoice
@@ -140,6 +109,9 @@ namespace Merchello.Core.Models
             return gatewayProviderService.GetAppliedPaymentsByInvoiceKey(invoice.Key);
         }
 
+        #endregion
+
+        #region Payments
 
         /// <summary>
         /// Attempts to process a payment
@@ -392,5 +364,47 @@ namespace Merchello.Core.Models
             var paymentGatewayMethod = merchelloContext.Gateways.Payment.GetPaymentGatewayMethodByKey(paymentMethodKey);
             return invoice.RefundPayment(payment, paymentGatewayMethod, args);
         }
+
+        #endregion
+
+
+        #region Taxation
+
+        /// <summary>
+        /// Calculates taxes for the invoice
+        /// </summary>
+        /// <param name="invoice">The <see cref="IInvoice"/></param>
+        /// <returns>The <see cref="ITaxCalculationResult"/> from the calculation</returns>
+        public static ITaxCalculationResult CalculateTaxes(this IInvoice invoice)
+        {
+            return invoice.CalculateTaxes(invoice.GetBillingAddress());
+        }
+
+        /// <summary>
+        /// Calculates taxes for the invoice
+        /// </summary>
+        /// <param name="invoice">The <see cref="IInvoice"/></param>
+        /// <param name="taxAddress">The address (generally country code and region) to be used to determine the taxation rates</param>
+        /// <returns>The <see cref="ITaxCalculationResult"/> from the calculation</returns>
+        public static ITaxCalculationResult CalculateTaxes(this IInvoice invoice, IAddress taxAddress)
+        {
+            return invoice.CalculateTaxes(MerchelloContext.Current, taxAddress);
+        }
+
+        /// <summary>
+        /// Calculates taxes for the invoice
+        /// </summary>
+        /// <param name="invoice">The <see cref="IInvoice"/></param>
+        /// <param name="merchelloContext">The <see cref="IMerchelloContext"/></param>
+        /// <param name="taxAddress">The address (generally country code and region) to be used to determine the taxation rates</param>
+        /// <returns>The <see cref="ITaxCalculationResult"/> from the calculation</returns>
+        public static ITaxCalculationResult CalculateTaxes(this IInvoice invoice, IMerchelloContext merchelloContext, IAddress taxAddress)
+        {
+            // remove any other tax lines
+            return merchelloContext.Gateways.Taxation.CalculateTaxesForInvoice(invoice, taxAddress);
+        }
+
+        #endregion
+        
     }
 }
