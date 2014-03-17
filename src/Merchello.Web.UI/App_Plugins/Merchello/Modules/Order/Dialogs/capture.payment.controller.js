86963fd973141052f0fede603b39b22b6df1de08
Jason Prothero <jason@proworks.com>
2014-03-17 13:22:30 -0700

    added dialogs to OrderView

---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Order/Dialogs/capture.payment.controller.js
@@ -0,0 +1,19 @@
+﻿(function (controllers, undefined) {
+
+    /**
+     * @ngdoc controller
+     * @name Merchello.Dashboards.Order.Dialogs.CapturePaymentController
+     * @function
+     * 
+     * @description
+     * The controller for the capturing of authorized payments on the Order View page
+     */
+    controllers.CapturePaymentController = function ($scope) {
+
+
+    };
+
+    angular.module("umbraco").controller("Merchello.Dashboards.Order.Dialogs.CapturePaymentController", merchello.Controllers.CapturePaymentController);
+
+
+}(window.merchello.Controllers = window.merchello.Controllers || {}));
