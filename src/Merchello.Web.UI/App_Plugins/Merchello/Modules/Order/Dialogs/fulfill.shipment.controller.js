26f45ee9ba5b91ba89f79f6540824b5a0a6468dc
Jason Prothero <jason@proworks.com>
2014-03-17 22:20:06 -0700

    fulfillment dialog wireup

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Order/Dialogs/fulfill.shipment.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Order/Dialogs/fulfill.shipment.controller.js
@@ -8,9 +8,43 @@
      * @description
      * The controller for the fulfillment of shipments on the Order View page
      */
-    controllers.FulfillShipmentController = function ($scope) {
+    controllers.FulfillShipmentController = function ($scope, merchelloOrderService, notificationsService) {
 
+        //--------------------------------------------------------------------------------------
+        // Initialization methods
+        //--------------------------------------------------------------------------------------
 
+        $scope.getUnFulfilledItems = function (orderKey) {
+
+            var promise = merchelloOrderService.getUnFulfilledItems(orderKey);
+
+            promise.then(function (items) {
+
+                $scope.dialogData.items = _.map(items, function (item) {
+                    return new merchello.Models.OrderLineItem(item);
+                });
+
+            }, function (reason) {
+                notificationsService.error("Line Items Load Failed", reason.message);
+            });
+        };
+
+        /**
+         * @ngdoc method
+         * @name init
+         * @function
+         * 
+         * @description
+         * Method called on intial page load.  Loads in data from server and sets up scope.
+         */
+        $scope.init = function () {
+
+            $scope.getUnFulfilledItems($scope.dialogData.key);
+            $scope.dialogData.trackingNumber = "";
+
+        };
+
+        $scope.init();
     };
 
     angular.module("umbraco").controller("Merchello.Dashboards.Order.Dialogs.FulfillShipmentController", merchello.Controllers.FulfillShipmentController);
