973c526a04bbef63e1eeb50aeb55d9593fe3a793
Rusty Swayne <rusty@mindfly.com>
2014-04-21 09:28:29 -0700

    AuthorizeNet build

--- Plugin/Payments/AuthorizeNet/src/Merchello.Plugin.Payments.AuthorizeNet/Properties/VersionInfo.cs
+++ Plugin/Payments/AuthorizeNet/src/Merchello.Plugin.Payments.AuthorizeNet/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.17.40338")]
+[assembly: AssemblyFileVersion("1.0.20.25092")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
