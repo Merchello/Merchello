0bc301d0dcdfaaa35f04ab9bc4e9ee92ed3aa37f
Mark Bowser <mark@proworks.com>
2014-04-22 16:37:51 -0700

    Settings\Shipping using umbraco dialogService instead of custom flyout

---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/Shipping/Dialogs/shipping.deletewarehouse.controller.js
@@ -0,0 +1,18 @@
+﻿(function (controllers, undefined) {
+
+    /**
+     * @ngdoc controller
+     * @name Merchello.Dashboards.Settings.Shipping.Dialogs.ShippingDeleteWarehouseController
+     * @function
+     * 
+     * @description
+     * The controller for deleting a warehouse
+     */
+	controllers.ShippingDeleteWarehouseController = function ($scope) {
+
+    };
+
+	angular.module("umbraco").controller("Merchello.Dashboards.Settings.Shipping.Dialogs.ShippingDeleteWarehouseController", ['$scope', merchello.Controllers.ShippingDeleteWarehouseController]);
+
+
+}(window.merchello.Controllers = window.merchello.Controllers || {}));
---
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/Shipping/Dialogs/shipping.editwarehouse.controller.js
@@ -0,0 +1,18 @@
+﻿(function (controllers, undefined) {
+
+    /**
+     * @ngdoc controller
+     * @name Merchello.Dashboards.Settings.Shipping.Dialogs.ShippingEditWarehouseController
+     * @function
+     * 
+     * @description
+     * The controller for adding / editing a warehouse
+     */
+	controllers.ShippingEditWarehouseController = function ($scope) {
+
+    };
+
+	angular.module("umbraco").controller("Merchello.Dashboards.Settings.Shipping.Dialogs.ShippingEditWarehouseController", ['$scope', merchello.Controllers.ShippingEditWarehouseController]);
+
+
+}(window.merchello.Controllers = window.merchello.Controllers || {}));
