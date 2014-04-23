81009909ed0ecfb54bba38eb158832eaf5c364a1
Rusty Swayne <rusty@mindfly.com>
2014-04-23 14:08:52 -0700

    #M-217 Fixed

---
+++ src/Merchello.Web/AutoMapperMappings-Payment.cs
@@ -0,0 +1,45 @@
+﻿using Merchello.Core.Gateways.Payment;
+using Merchello.Core.Models;
+using Merchello.Web.Models.ContentEditing;
+using Merchello.Web.Models.MapperResolvers;
+
+namespace Merchello.Web
+{
+    /// <summary>
+    /// Binds Merchello AutoMapper mappings during the Umbraco startup.
+    /// </summary>
+    internal  static partial class AutoMapperMappings
+    {
+        private static void BindPaymentMappings()
+        {
+            // payment
+            AutoMapper.Mapper.CreateMap<IAppliedPayment, AppliedPaymentDisplay>();
+
+            AutoMapper.Mapper.CreateMap<IPayment, PaymentDisplay>();
+
+            AutoMapper.Mapper.CreateMap<IPaymentMethod, PaymentMethodDisplay>();
+
+            AutoMapper.Mapper.CreateMap<IPaymentGatewayMethod, PaymentMethodDisplay>()
+                .ForMember(dest => dest.Key,
+                    opt => opt.MapFrom(src => src.PaymentMethod.Key)
+                )
+                .ForMember(dest => dest.ProviderKey,
+                    opt => opt.MapFrom(src => src.PaymentMethod.ProviderKey)
+                )
+                .ForMember(dest => dest.Name,
+                    opt => opt.MapFrom(src => src.PaymentMethod.Name)
+                )
+                .ForMember(dest => dest.Description,
+                    opt => opt.MapFrom(src => src.PaymentMethod.Description)
+                )
+                .ForMember(dest => dest.PaymentCode,
+                    opt => opt.MapFrom(src => src.PaymentMethod.PaymentCode)
+                )
+                .ForMember(dest => dest.DialogEditorView,
+                    opt => opt.ResolveUsing<GatewayMethodDialogEditorViewResolver>()
+                        .ConstructedBy(() => new GatewayMethodDialogEditorViewResolver())
+                );
+
+        }
+    }
+}
