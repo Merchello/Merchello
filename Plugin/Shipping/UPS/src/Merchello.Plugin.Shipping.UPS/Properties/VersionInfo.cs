3f2b0ccb8901253f5a0c331271fa6b9ffc38f20d
koswesley <wesley@proworks.com>
2014-05-07 14:28:41 -0700

    Shipping Methods

--- Plugin/Shipping/UPS/src/Merchello.Plugin.Shipping.UPS/Properties/VersionInfo.cs
+++ Plugin/Shipping/UPS/src/Merchello.Plugin.Shipping.UPS/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.35.29747")]
+[assembly: AssemblyFileVersion("1.0.36.38018")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
