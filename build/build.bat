4a33830294d0783e533852090b4f0a001edf4601
Tom Fulton <tomnf1@gmail.com>
2014-05-09 14:39:31 -0600

    Fix build script to work with both VS12 and 13

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,15 @@
 @ECHO OFF
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Merchello.build" /target:Build
+SET MSBuildLoc=
+SET MSBuild_VS12=C:\windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+SET MSBuild_VS13_32="C:\Program Files\MSBuild\12.0\bin\msbuild.exe"
+SET MSBuild_VS13_64="C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe"
+
+if exist %MSBuild_VS12% SET MSBuildLoc=%MSBuild_VS12%
+if exist %MSBuild_VS13_32% SET MSBuildLoc=%MSBuild_VS13_32%
+if exist %MSBuild_VS13_64% SET MSBuildLoc=%MSBuild_VS13_64%
+
+%MSBuildLoc% "Merchello.build" /target:Build
 
 IF ERRORLEVEL 1 GOTO :showerror
 
