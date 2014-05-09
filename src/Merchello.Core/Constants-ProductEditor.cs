12111a8802d32a0d1244e7e0040fb436e7fad5e4
Tom Fulton <tomnf1@gmail.com>
2014-05-09 16:59:27 -0600

    Add virtual tab placeholder for new Product/Content editing
    * Re-create the IContent->ContentItemDisplay map so we can add a custom AfterMap event (probably a hack?)
    * Check for existence of merchProductKey property before adding tab
    * Add virtual tab and property (placeholder for now)

---
+++ src/Merchello.Core/Constants-ProductEditor.cs
@@ -0,0 +1,14 @@
+﻿namespace Merchello.Core
+{
+    public partial class Constants
+    {
+        public static class ProductEditor
+        {
+            public static string ProductKeyPropertyAlias = "merchProductKey";
+            public static string TabAlias = "merchProductEditor";
+            public static string TabLabel = "Merchello Product";
+            public static int TabId = -666; // TODO: What ID should we use?
+            public static string PropertyAlias = "merchProductEditor";
+        }
+    }
+}
---
+++ src/Merchello.Web/ProductContentEditing.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Merchello.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models.ContentEditing;
+
+namespace Merchello.Web
+{
+    internal class ProductContentEditing
+    {
+        internal static void BindMappings()
+        {
+            Mapper.CreateMap<IContent, ContentItemDisplay>().AfterMap(HandleAfterMap);
+        }
+
+        internal static void HandleAfterMap(IContent content, ContentItemDisplay contentItem)
+        {
+            if (ShouldAddProductEditor(contentItem))
+                AddTab(contentItem);
+        }
+
+        private static void AddTab(ContentItemDisplay contentItem)
+        {
+            var tab = new Tab<ContentPropertyDisplay>();
+            tab.Alias = Constants.ProductEditor.TabAlias;
+            tab.Label = Constants.ProductEditor.TabLabel;
+            tab.Id = Constants.ProductEditor.TabId;
+            tab.IsActive = true;
+
+            AddProperties(ref tab);
+
+            //Is there a better way?
+            var tabs = new List<Tab<ContentPropertyDisplay>>();
+            tabs.Add(tab);
+            tabs.AddRange(contentItem.Tabs);
+            contentItem.Tabs = tabs;
+        }
+
+        private static bool ShouldAddProductEditor(ContentItemDisplay contentItem)
+        {
+            return contentItem.Properties.Any(p => p.Alias == Constants.ProductEditor.ProductKeyPropertyAlias);
+        }
+
+        private static void AddProperties(ref Tab<ContentPropertyDisplay> tab)
+        {
+            var listViewProperties = new List<ContentPropertyDisplay>();
+            listViewProperties.Add(new ContentPropertyDisplay
+            {
+                Alias = string.Concat(Umbraco.Core.Constants.PropertyEditors.InternalGenericPropertiesPrefix, Constants.ProductEditor.PropertyAlias), // Must prefix with _umb_ so Umbraco knows not to save this property
+                Label = "",
+                Value = null,
+                View = "boolean",
+                HideLabel = true,
+                Config = new Dictionary<string, object>()
+            });
+            tab.Properties = listViewProperties;
+        }
+    }
+}
