05f25ba37fa49563592f50c1a749477369e8218c
Rusty Swayne <rusty@mindfly.com>
2014-05-15 16:23:13 -0700

    Updated NuGet reference to pull from MyGet

--- src/Merchello.Core/Gateways/Notification/Smtp/SmtpNotificationGatewayProvider.cs
+++ src/Merchello.Core/Gateways/Notification/Smtp/SmtpNotificationGatewayProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Merchello.Core.Models;
 using Merchello.Core.Services;
 using Umbraco.Core.Cache;
@@ -9,16 +10,26 @@ namespace Merchello.Core.Gateways.Notification.Smtp
      /// Represents the SMTP Notification Gateway Provider
      ///</summary>
     [GatewayProviderActivation("5F2E88D1-6D07-4809-B9AB-D4D6036473E9", "SMTP Notification Provider", "SMTP Notification Provider")]
-    [GatewayProviderEditor("AuthorizeNet configuration", "~/App_Plugins/Merchello/Modules/Settings/Notifications/Dialog/smtp.notifications.providersettings.html")]
+    [GatewayProviderEditor("SMTP Notification Configuration", "~/App_Plugins/Merchello/Modules/Settings/Notifications/Dialog/smtp.notifications.providersettings.html")]
     public class SmtpNotificationGatewayProvider : NotificationGatewayProviderBase, ISmtpNotificationGatewayProvider
     {
+        #region Resources
+        
+        private static readonly IEnumerable<IGatewayResource> AvailableResources = new List<IGatewayResource>()
+        {
+            new GatewayResource("Email", "Email Notification")
+        };
+
+        #endregion
+
         public SmtpNotificationGatewayProvider(IGatewayProviderService gatewayProviderService, IGatewayProviderSettings gatewayProviderSettings, IRuntimeCacheProvider runtimeCacheProvider) 
             : base(gatewayProviderService, gatewayProviderSettings, runtimeCacheProvider)
         { }
 
          public override IEnumerable<IGatewayResource> ListResourcesOffered()
          {
-             throw new System.NotImplementedException();
+             return AvailableResources.Where(x => NotificationMethods.Any(y => y.ServiceCode != x.ServiceCode));
          }
+
     }
 }
