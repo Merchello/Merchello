2513958e9d5d095684df235d49a4bc4f9456e050
Kyle Weems <kyle@mindfly.com>
2014-06-04 12:03:35 -0700

    Added binding functionality to the Gateway Providers > Notification Providers edit flyout.

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/GatewayProviders/Dialogs/smtp.notifications.providersettings.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Settings/GatewayProviders/Dialogs/smtp.notifications.providersettings.controller.js
@@ -10,6 +10,35 @@
      */
     controllers.SmtpNotificationsProviderSettingsController = function ($scope) {
 
+        /**
+        * @ngdoc method 
+        * @name init
+        * @function
+        * 
+        * @description
+        * Method called on intial page load.  Loads in data from server and sets up scope.
+        */
+        $scope.init = function () {
+
+            //$scope.dialogData.provider.extendedData
+
+            // on initial load extendedData will be empty but we need to populate with key values
+            // 
+            if ($scope.dialogData.provider.extendedData.length > 0) {
+                var settingsString = $scope.dialogData.provider.extendedData[0].value;
+                $scope.notificationProviderSettings = JSON.parse(settingsString);
+                console.info($scope.dialogData);
+                console.info($scope.notificationProviderSettings);
+
+                // Watch with object equality to convert back to a string for the submit() call on the Save button
+                $scope.$watch(function () {
+                    return $scope.notificationProviderSettings;
+                }, function (newValue, oldValue) {
+                    $scope.dialogData.provider.extendedData[0].value = angular.toJson(newValue);
+                }, true);
+            }
+        };
+        $scope.init();
 
     };
 
