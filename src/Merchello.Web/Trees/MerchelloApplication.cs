48556a6b1f8907b2eee3fb681d9525a4e075c27e
Rusty Swayne <rusty@mindfly.com>
2014-06-20 14:46:44 -0700

    #M-278 Fixed

--- src/Merchello.Web/Trees/MerchelloApplication.cs
+++ src/Merchello.Web/Trees/MerchelloApplication.cs
@@ -1,9 +1,9 @@
-﻿using umbraco.businesslogic;
-using umbraco.interfaces;
-using Umbraco.Web.Mvc;
-
-namespace Merchello.Web.Trees
+﻿namespace Merchello.Web.Trees
 {
+    using umbraco.businesslogic;
+    using umbraco.interfaces;
+    using Umbraco.Web.Mvc;
+
     [Application("merchello", "Merchello", "Merchello-Icon.png", 10)] // coin
     [PluginController("Merchello")]
     public class MerchelloApp : IApplication
