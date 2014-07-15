d9c6ee7ad5ebef438c310146f61c13bfe07d55ab
Gary <gary@proworks.com>
2014-07-15 09:31:58 -0700

    Add New WebApi method: RemoveAnonymousCustomers

---
+++ src/Merchello.Web/WebApi/ScheduledTasksController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Linq;
+using Umbraco.Web.Mvc;
+using Merchello.Core;
+using Merchello.Core.Models;
+using Merchello.Core.Services;
+using Merchello.Core.Configuration;
+using Merchello.Web.WebApi;
+using Umbraco.Web;
+using Merchello.Web.Models.ContentEditing;
+using Examine;
+using Umbraco.Web.WebApi;
+
+namespace Merchello.Web.WebApi
+{
+    /// <summary>
+    /// Schedule Tasks
+    /// </summary>
+    //[PluginController("Merchello")]
+    public class ScheduledTasksApiController : UmbracoApiController
+    {
+        /// <summary>
+        /// Delete all customers older than the date in the setting
+        /// 
+        /// GET /umbraco/Merchello/ScheduledTasksApi/RemoveAnonymousCustomers
+        /// </summary>
+        [AcceptVerbs("GET", "POST")]
+        public int RemoveAnonymousCustomers()
+        {
+            int maxDays = MerchelloConfiguration.Current.AnonymousCustomersMaxDays;
+
+            var repo = new AnonymousCustomerService();
+
+            var anonymousCustomers = repo.GetAnonymousCustomersCreatedBefore(DateTime.Now); //.AddDays(-maxDays));
+
+            repo.Delete(anonymousCustomers);
+
+            return anonymousCustomers.Count();
+        }
+    }
+}
