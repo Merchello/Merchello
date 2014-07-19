89b171b454b4b8275665cf528d5c576854fafff5
Kyle Weems <kyle@mindfly.com>
2014-07-19 12:41:25 -0700

    Added functionality and view bindings for creating/editing billing address on customer edit view/controller.

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Customer/customer.edit.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Customer/customer.edit.controller.js
@@ -12,20 +12,42 @@
 
         /**
          * @ngdoc method
-         * @name buildBillingAddressList
+         * @name buildAddressList
          * @function
          * 
          * @description
-         * Build a list of billing addresses. 
+         * Add an address to the customer's list of addresses. 
          */
-        $scope.buildBillingAddressList = function (addresses) {
+        $scope.addAddress = function (type) {
+            // TODO: Add functionality to tag address with applicable billing or shipping flags.
+            var newAddress;
+            if (type === 'billing') {
+                newAddress = $scope.currentBillingAddress;
+            } else {
+                newAddress = $scope.currentShippingAddress;
+            }
+            newAddress.customerKey = $scope.customer.key;
+            newAddress.addressType = type;
+            $scope.customer.addresses.push(newAddress);
+            notificationsService.success("Address added to list. Customer must be saved to keep change", "");
+        };
+
+        /**
+         * @ngdoc method
+         * @name buildAddressList
+         * @function
+         * 
+         * @description
+         * Build a list of addresses and assign to either billing or shipping address lists depending on passed parameters. 
+         */
+        $scope.buildAddressList = function (type, addresses) {
             // TODO: Modify this function to seperate out only billing addresses once there exists typefieldkeys to match against
-            $scope.billingAddresses = [];
+            var addressList = [];
             var defaultAddress = false;
             if (addresses.length > 0) {
                 for (var i = 0; i < addresses.length; i++) {
                     var newAddress = new merchello.Models.CustomerAddress(addresses[i]);
-                    $scope.billingAddresses.push(newAddress);
+                    addressList.push(newAddress);
                     if (newAddress.isDefault) {
                         defaultAddress = newAddress;
                     }
@@ -33,66 +55,32 @@
                 if (!defaultAddress) {
                     defaultAddress = addresses[0];
                 }
-                $scope.currentBillingAddress = defaultAddress;
+                if (type === 'billing') {
+                    $scope.currentBillingAddress = defaultAddress;
+                    $scope.billingAddresses = addressList;
+                } else {
+                    $scope.currentShippingAddress = defaultAddress;
+                    $scope.shippingAddresses = addressList;
+                }
             }
         };
 
         /**
          * @ngdoc method
-         * @name buildCountryList
+         * @name doesCountryHaveProvinces
          * @function
          * 
          * @description
-         * Build a list of countries. 
+         * Returns true if the country provided has provinces. Otherwise returns false.
          */
-        $scope.buildCountryList = function () {
-            // TODO: Modify this someday to require less calls to do the same thing.
-            $scope.countries = [{id: -1, countryCode: 00, name: 'Select Country'}];
-            var promiseWarehouse = merchelloWarehouseService.getDefaultWarehouse();
-            promiseWarehouse.then(function (warehouseResponse) {
-                var catalogKey = warehouseResponse.warehouseCatalogs[0].key;
-                if (catalogKey) {
-                    var promiseCatalog = merchelloCatalogShippingService.getWarehouseCatalogShippingCountries(catalogKey);
-                    promiseCatalog.then(function (promiseCountries) {
-                        for (var i = 0; i < promiseCountries.length; i++) {
-                            var newCountry = {
-                                id: i,
-                                countryCode: promiseCountries[i].countryCode,
-                                name: promiseCountries[i].name
-                            };
-                            $scope.countries.push(newCountry);
-                        }
-                        $scope.filters.country = $scope.countries[0];
-                    });
+        $scope.doesCountryHaveProvinces = function(country) {
+            var result = false;
+            if (country.provinces) {
+                if (country.provinces.length > 0) {
+                    result = true;
                 }
-            });
-        }
-
-        /**
-         * @ngdoc method
-         * @name buildBillingAddressList
-         * @function
-         * 
-         * @description
-         * Build a list of shipping addresses. 
-         */
-        $scope.buildShippingAddressList = function (addresses) {
-            // TODO: Modify this function to seperate out only shipping addresses once there exists typefieldkeys to match against
-            $scope.shippingAddresses = [];
-            var defaultAddress = false;
-            if (addresses.length > 0) {
-                for (var i = 0; i < $scope.addresses.length; i++) {
-                    var newAddress = new merchello.Models.CustomerAddress(addresses[i]);
-                    $scope.shippingAddresses.push(newAddress);
-                    if (newAddress.isDefault) {
-                        defaultAddress = newAddress;
-                    }
-                }
-                if (!defaultAddress) {
-                    defaultAddress = addresses[0];
-                }
-                $scope.currentShippingAddress = defaultAddress;
             }
+            return result;
         };
 
         /**
@@ -103,10 +91,10 @@
          * @description
          * Inititalizes the scope.
          */
-        $scope.init = function() {
+        $scope.init = function () {
             $scope.setVariables();
             $scope.loadCustomer();
-            $scope.buildCountryList();
+            $scope.loadCountries();
         };
 
         /**
@@ -143,6 +131,36 @@
 
         /**
          * @ngdoc method
+         * @name loadCountries
+         * @function
+         * 
+         * @description
+         * Load a list of countries and provinces from the API. 
+         */
+        $scope.loadCountries = function () {
+            $scope.countries = [];
+            var promiseCountries = merchelloSettingsService.getAllCountries();
+            promiseCountries.then(function (countriesResponse) {
+                for (var i = 0; i < countriesResponse.length; i++) {
+                    var country = countriesResponse[i];
+                    var newCountry = {
+                        id: i,
+                        countryCode: country.countryCode,
+                        name: country.name,
+                        provinces: country.provinces,
+                        provinceLabel: country.provinceLabel
+                    };
+                    $scope.countries.push(newCountry);
+                }
+                $scope.countries.sort($scope.sortCountries);
+                $scope.countries.unshift({ id: -1, name: 'Select Country', countryCode: '00', provinces: {}, provinceLabel: '' });
+                $scope.filters.billingCountry = $scope.countries[0];
+                $scope.filters.ShippingCountry = $scope.countries[0];
+            });
+        };
+
+        /**
+         * @ngdoc method
          * @name loadCustomer
          * @function
          * 
@@ -158,8 +176,8 @@
                 promiseLoadCustomer.then(function(customerResponse) {
                     $scope.customer = new merchello.Models.Customer(customerResponse);
                     $scope.updateAvatarUrl();
-                    $scope.buildBillingAddressList(customerResponse.addresses);
-                    $scope.buildShippingAddressList(customerResponse.addresses);
+                    $scope.buildAddressList('billing', customerResponse.addresses);
+                    $scope.buildAddressList('shipping', customerResponse.addresses);
                     $scope.loaded = true;
                 }, function(reason) {
                     notificationsService.error("Failed to load customer", reason.message);
@@ -216,12 +234,34 @@
             $scope.currentShippingAddress = new merchello.Models.CustomerAddress();
             $scope.customer = new merchello.Models.Customer();
             $scope.filters = {
-                country: {}
+                billingCountry: {},
+                billingProvnce: {},
+                shippingCountry: {},
+                shippingProvince: {}
             };
             $scope.loaded = false;
             $scope.provinces = [];
+            $scope.provinceLabel = "State/Province";
             $scope.shippingAddresses = [];
             $scope.wasFormSubmitted = false;
+        };
+
+        /**
+         * @ngdoc method
+         * @name sortCountries
+         * @function
+         * 
+         * @description
+         * Helper function that can be called by $scope.countries.sort($scope.sortCountries) to alphabetically sort countries.
+         */
+        $scope.sortCountries = function(a, b) {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
         };
 
         /**
@@ -235,6 +275,64 @@
         $scope.updateAvatarUrl = function () {
             $scope.avatarUrl = merchelloGravatarService.avatarUrl($scope.customer.email);
         };
+
+        /**
+         * @ngdoc method
+         * @name updateCountry
+         * @function
+         * 
+         * @description
+         * Update the selected country for the applicable address type, and prepare the provinces for selection.
+         */
+        $scope.updateCountry = function(type) {
+            var selectedCountry;
+            if (type === 'billing') {
+                selectedCountry = $scope.filters.billingCountry;
+                if (selectedCountry.id > -1) {
+                    $scope.currentBillingAddress.countryCode = selectedCountry.countryCode;
+                    if (selectedCountry.provinces.length > 0) {
+                        $scope.currentBillingAddress.region = '';
+                        if (selectedCountry.provinces[0].code !== '00') {
+                            $scope.filters.billingCountry.provinces.unshift({ code: '00', name: 'Select State/Province' });
+                        }
+                        $scope.filters.billingProvince = $scope.filters.billingCountry.provinces[0];
+                    }
+                }
+            } else {
+                selectedCountry = $scope.filters.shippingCountry;
+                if (selectedCountry.id > -1) {
+                    $scope.currentShippingAddress.countryCode = selectedCountry.countryCode;
+                    if (selectedCountry.provinces.length > 0) {
+                        $scope.currentShippingAddress.region = '';
+                        $scope.filters.shippingCountry.provinces.unshift({ code: '00', name: 'Select State/Province' });
+                        $scope.filters.shippingProvince = $scope.filters.shippingCountry.provinces[0];
+                    }
+                }
+            }
+        }
+
+        /**
+         * @ngdoc method
+         * @name updateProvince
+         * @function
+         * 
+         * @description
+         * Update the selected province for the applicable address type.
+         */
+        $scope.updateProvince = function (type) {
+            var selectedProvince;
+            if (type === 'billing') {
+                selectedProvince = $scope.filters.billingProvince;
+                if (selectedProvince.code !== '00') {
+                    $scope.currentBillingAddress.region = selectedProvince.name;
+                }
+            } else {
+                selectedProvince = $scope.filters.shippingProvince;
+                if (selectedProvince.code !== '00') {
+                    $scope.currentShippingAddress.region = selectedProvince.name;
+                }
+            }
+        }
 
         $scope.init();
 
