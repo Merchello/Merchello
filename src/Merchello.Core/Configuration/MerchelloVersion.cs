5c1e0c8bbbf2f1a65ff4eee1e0046627f1d95fc2
Rusty Swayne <rusty@mindfly.com>
2014-07-22 16:13:01 -0700

    EOD check in.

--- src/Merchello.Core/Configuration/MerchelloVersion.cs
+++ src/Merchello.Core/Configuration/MerchelloVersion.cs
@@ -12,7 +12,7 @@
         /// <summary>
         /// The version.
         /// </summary>
-        private static readonly Version Version = new Version("1.2.10");
+        private static readonly Version Version = new Version("1.2.11");
 
         /// <summary>
         /// Gets the current version of Merchello.
