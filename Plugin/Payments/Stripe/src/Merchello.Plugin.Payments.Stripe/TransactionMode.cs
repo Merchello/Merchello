8eeeeb9ee8ddfea5d621b197ead5a84519b5f919
Alex Lindgren <alindgren@flightpath.com>
2014-07-28 22:53:47 -0400

    Stripe payment provider

---
+++ Plugin/Payments/Stripe/src/Merchello.Plugin.Payments.Stripe/TransactionMode.cs
@@ -0,0 +1,18 @@
+﻿namespace Merchello.Plugin.Payments.Stripe
+{
+    /// <summary>
+    /// Represents Stripe payment processor transaction mode
+    /// </summary>
+    public enum TransactionMode
+    {
+        /// <summary>
+        /// An Authorize transaction
+        /// </summary>
+        Authorize,
+
+        /// <summary>
+        /// An Authorize and Capture transaction
+        /// </summary>
+        AuthorizeAndCapture
+    }
+}
