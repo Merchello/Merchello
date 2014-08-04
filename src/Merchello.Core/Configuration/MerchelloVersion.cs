99f33c97dd8651444f2ad14907b691179f2cdc9b
Rusty Swayne <rusty@mindfly.com>
2014-08-04 15:00:20 -0700

    Updates Version to 1.3.0

--- src/Merchello.Core/Configuration/MerchelloVersion.cs
+++ src/Merchello.Core/Configuration/MerchelloVersion.cs
@@ -12,7 +12,7 @@
         /// <summary>
         /// The version.
         /// </summary>
-        private static readonly Version Version = new Version("1.2.21");
+        private static readonly Version Version = new Version("1.3.0");
 
         /// <summary>
         /// Gets the current version of Merchello.
