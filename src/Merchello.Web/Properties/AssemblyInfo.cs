c9cc7ce9b215a5ae31668a448a29e6be6a6040b5
Rusty Swayne <rusty@mindfly.com>
2014-08-04 14:31:48 -0700

    Updated PayPal Provider to current Merchello version.

--- src/Merchello.Web/Properties/AssemblyInfo.cs
+++ src/Merchello.Web/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.2.20")]
-[assembly: AssemblyFileVersion("1.2.20")]
-[assembly: AssemblyInformationalVersion("1.2.20")]
+[assembly: AssemblyVersion("1.2.21")]
+[assembly: AssemblyFileVersion("1.2.21")]
+[assembly: AssemblyInformationalVersion("1.2.21")]
 
 
