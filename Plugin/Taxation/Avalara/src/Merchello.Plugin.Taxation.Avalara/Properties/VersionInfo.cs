f51e3ae0dcd84b8bca46024d83d97e7f176e6da5
Rusty Swayne <rusty@mindfly.com>
2014-08-08 08:31:34 -0700

    commit

--- Plugin/Taxation/Avalara/src/Merchello.Plugin.Taxation.Avalara/Properties/VersionInfo.cs
+++ Plugin/Taxation/Avalara/src/Merchello.Plugin.Taxation.Avalara/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.128.42197")]
+[assembly: AssemblyFileVersion("1.0.128.44517")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
