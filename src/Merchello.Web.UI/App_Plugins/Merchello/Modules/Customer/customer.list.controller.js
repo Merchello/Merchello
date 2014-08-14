ac6f52a47333a33618d8be0b127ee726a544237b
Kyle Weems <kyle@mindfly.com>
2014-08-14 15:57:01 -0700

    Starting new function to merge filtered and unfiltered customer listings on the Customer List page.

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Customer/customer.list.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Customer/customer.list.controller.js
@@ -12,27 +12,6 @@
 
         /**
          * @ngdoc method
-         * @name getAllCustomers
-         * @function
-         * 
-         * @description
-         * Load all the customers from the API.
-         */
-        $scope.getAllCustomers = function() {
-            var promiseAllCustomers = merchelloCustomerService.GetAllCustomers();
-            promiseAllCustomers.then(function (customersResponse) {
-                if (customersResponse) {
-                    $scope.customers = [];
-                    var queryResult = new merchello.Models.QueryResult(customersResponse);
-                    $scope.customers = _.map(queryResult.items, function (customer) {
-                        return new merchello.Models.Customer(customer);
-                    });
-                }
-            });
-        };
-
-        /**
-         * @ngdoc method
          * @name filterCustomers
          * @function
          * 
@@ -45,6 +24,53 @@
 
         /**
          * @ngdoc method
+         * @name getAllCustomers
+         * @function
+         * 
+         * @description
+         * Load all the customers from the API.
+         */
+        $scope.getAllCustomers = function () {
+            var promiseAllCustomers = merchelloCustomerService.GetAllCustomers();
+            promiseAllCustomers.then(function (customersResponse) {
+                if (customersResponse) {
+                    $scope.customers = [];
+                    var queryResult = new merchello.Models.QueryResult(customersResponse);
+                    $scope.customers = _.map(queryResult.items, function (customer) {
+                        return new merchello.Models.Customer(customer);
+                    });
+                }
+            });
+        };
+
+        $scope.getCustomers = function(filterText) {
+            var page = $scope.currentPage;
+            var perPage = $scope.perPage;
+            $scope.filterText = filterText;
+            var promiseCustomers;
+            if (filterText === undefined) {
+                filterText = '';
+            }
+            if (filterText === '') {
+                promiseCustomers = merchelloCustomerService.GetAllCustomers(page, perPage);
+            } else {
+                promiseCustomers = merchelloCustomerService.getFiltered(filterText, page, perPage);
+            }
+            promiseCustomers.then(function (response) {
+                if (response) {
+                    $scope.customers = [];
+                    var queryResult = new merchello.Models.QueryResult(response);
+                    $scope.customers = _.map(queryResult.items, function (customer) {
+                        return new merchello.Models.Customer(customer);
+                    });
+                }
+            }, function (reason) {
+                notificationsService.error("Failed To Load Customers", reason.message);
+            });
+        };
+
+        /**
+         * @ngdoc method
          * @name init
          * @function
          * 
@@ -53,7 +79,7 @@
          */
         $scope.init = function () {
             $scope.setVariables();
-            $scope.getAllCustomers();
+            $scope.getCustomers();
         };
 
         /**
@@ -112,6 +138,8 @@
          * Sets $scope variables.
          */
         $scope.setVariables = function () {
+            $scope.currentPage = 0;
+            $scope.perPage = 10;
             $scope.customers = [];
             $scope.loaded = true;
             $scope.preValuesLoaded = true;
