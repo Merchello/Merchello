19e3cff9133771b1f847229767b97f388a2d5dfb
Kyle Weems <kyle@mindfly.com>
2014-08-27 15:31:19 -0700

    .

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Order/merchello.audit.service.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Order/merchello.audit.service.js
@@ -1,1 +1,32 @@
-﻿
+﻿(function (merchelloServices, undefined) {
+
+    /**
+    * @ngdoc service
+    * @name merchello.Services.MerchelloInvoiceService
+    * @description Loads in data and allows modification for invoices
+    **/
+    merchelloServices.MerchelloAuditService = function ($http, umbRequestHelper) {
+
+        return {
+
+            /**
+            * @ngdoc method
+            * @name getByKey
+            * @description 
+            **/
+            getSalesHistoryByInvoiceKey: function (key) {
+                return umbRequestHelper.resourcePromise(
+                    $http({
+                        url: umbRequestHelper.getApiUrl('merchelloInvoiceApiBaseUrl', 'GetInvoice'),
+                        method: "GET",
+                        params: { id: key }
+                    }),
+                    'Failed to retreive data for invoice id: ' + id);
+            }
+
+        };
+    };
+
+    angular.module('umbraco.resources').factory('merchelloAuditService', ['$http', 'umbRequestHelper', merchello.Services.MerchelloAuditService]);
+
+}(window.merchello.Services = window.merchello.Services || {}));
