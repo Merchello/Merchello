3a8f5832235b556c6bea61cb9facbe2d7550c167
Rusty Swayne <rusty@mindfly.com>
2014-08-28 10:18:13 -0700

    Added missed files

---
+++ src/Merchello.Web/Models/MapperResolvers/EntityTypeResolver.cs
@@ -0,0 +1,27 @@
+﻿namespace Merchello.Web.Models.MapperResolvers
+{
+    using AutoMapper;
+    using Core;
+    using Core.Models.Interfaces;
+    using Core.Models.TypeFields;
+
+    /// <summary>
+    /// The entity type resolver.
+    /// </summary>
+    public class EntityTypeResolver : ValueResolver<IAuditLog, EntityType>
+    {
+        /// <summary>
+        /// Resolves the EntityType Enumeration
+        /// </summary>
+        /// <param name="source">
+        /// The source.
+        /// </param>
+        /// <returns>
+        /// The <see cref="EntityType"/>.
+        /// </returns>
+        protected override EntityType ResolveCore(IAuditLog source)
+        {
+            return source.EntityTfKey != null ? EnumTypeFieldConverter.EntityType.GetTypeField(source.EntityTfKey.Value) : EntityType.Custom;
+        }
+    }
+}
