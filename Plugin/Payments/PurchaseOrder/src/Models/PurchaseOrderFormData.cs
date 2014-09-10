b6aec516e196f90895ab7869c366e5b69009da32
Wesley Kos <wesley@proworks.com>
2014-09-10 08:55:34 -0700

    Purchase Order Plugin Payment Provider

---
+++ Plugin/Payments/PurchaseOrder/src/Models/PurchaseOrderFormData.cs
@@ -0,0 +1,10 @@
+﻿namespace Merchello.Plugin.Payments.PurchaseOrder.Models
+{
+    public class PurchaseOrderFormData
+    {        
+        /// <summary>
+        /// The Purchase Orders Invoice Number.  
+        /// </summary>
+        public string PurchaseOrderNumber { get; set; }
+    }
+}
