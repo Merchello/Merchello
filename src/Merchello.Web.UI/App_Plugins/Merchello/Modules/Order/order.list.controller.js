0f42f4121c46db82ff8d02131f7d37be273ae74b
Wesley Kos <wesley@proworks.com>
2014-09-12 16:13:34 -0700

    download order csv

--- src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Order/order.list.controller.js
+++ src/Merchello.Web.UI/App_Plugins/Merchello/Modules/Order/order.list.controller.js
@@ -232,6 +232,51 @@
             $scope.visible.bulkActionDropdown = shouldShowDropdown;
         };
 
+        $scope.downloadOrderCSV = function() {
+            $scope.exportTableToCSV.apply(this, [$('#orderListTable>table'), 'export.csv']);
+        }
+
+        $scope.exportTableToCSV = function ($table, filename) {
+            var $rows = $table.find('tr:has(td)'),
+
+            // Temporary delimiter characters unlikely to be typed by keyboard
+            // This is to avoid accidentally splitting the actual contents
+            tmpColDelim = String.fromCharCode(11), // vertical tab character
+            tmpRowDelim = String.fromCharCode(0), // null character
+
+            // actual delimiter characters for CSV format
+            colDelim = '","',
+            rowDelim = '"\r\n"',
+
+            // Grab text from table into CSV formatted string
+            csv = '"' + $rows.map(function (i, row) {
+                var $row = $(row),
+                    $cols = $row.find('td');
+
+                return $cols.map(function (j, col) {
+                    var $col = $(col),
+                        text = $col.text();
+
+                    return text.replace('"', '""'); // escape double quotes
+
+                }).get().join(tmpColDelim);
+
+            }).get().join(tmpRowDelim)
+                .split(tmpRowDelim).join(rowDelim)
+                .split(tmpColDelim).join(colDelim) + '"',
+
+            // Data URI
+            csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+            $('#orderDownloadCSV')
+                .attr({
+                    'download': filename,
+                    'href': csvData,
+                    'target': '_blank'
+                });
+            
+        }
+
         $scope.init();
 
     };
