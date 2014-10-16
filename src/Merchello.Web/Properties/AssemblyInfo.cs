3c2fd3d952cf8086d2107ff6c2dac89f80a8b10b
Rusty Swayne <rusty@mindfly.com>
2014-10-16 16:24:25 -0700

    Adds extension method hack to reverse 7.1.7 patch following Umbraco's method

--- src/Merchello.Web/Properties/AssemblyInfo.cs
+++ src/Merchello.Web/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.4.10")]
-[assembly: AssemblyFileVersion("1.4.10")]
-[assembly: AssemblyInformationalVersion("1.4.10")]
+[assembly: AssemblyVersion("1.4.11")]
+[assembly: AssemblyFileVersion("1.4.11")]
+[assembly: AssemblyInformationalVersion("1.4.11")]
 
 
