422e805abf2fcf7d5db347bc65d08a2d4c7b513f
Rusty Swayne <rusty@mindfly.com>
2014-10-28 15:43:58 -0700

    Free Over Amount Shipping provider revisions

--- Plugin/Shipping/FreeOverAmount/src/Merchello.Plugin.Shipping.FOA/Properties/AssemblyInfo.cs
+++ Plugin/Shipping/FreeOverAmount/src/Merchello.Plugin.Shipping.FOA/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.210.36167")]
+[assembly: AssemblyFileVersion("1.0.210.42519")]
 [assembly: AssemblyInformationalVersion("1.0.1.0")]
 
 
