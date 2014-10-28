dbf66cb1eb87dd3794a00858dd712ee1a17b738e
Rusty Swayne <rusty@mindfly.com>
2014-10-28 07:12:00 -0700

    Adds extension methods to the Braintree provider.

--- Plugin/Payments/BrainTree/src/Merchello.Plugin.Payments.BrainTree/Properties/VersionInfo.cs
+++ Plugin/Payments/BrainTree/src/Merchello.Plugin.Payments.BrainTree/Properties/VersionInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.206.33186")]
-[assembly: AssemblyInformationalVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.209.35993")]
+[assembly: AssemblyInformationalVersion("1.0.4.0")]
 
 
