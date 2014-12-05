cafc558a434f23962345fcb082121a07364c2f98
Rusty Swayne <rusty@mindfly.com>
2014-12-05 15:25:43 -0800

    Sets patches the default list counts to be 25.

--- Plugin/Reports/SalesReports/src/Merchello.Reports.SalesReports/Properties/VersionInfo.cs
+++ Plugin/Reports/SalesReports/src/Merchello.Reports.SalesReports/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.33.37267")]
+[assembly: AssemblyFileVersion("1.0.34.34962")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
