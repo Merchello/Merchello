76199012218df587746c6c29e9aefd034baf2f21
Rusty Swayne <rusty@mindfly.com>
2014-12-18 10:40:44 -0800

    Formats TaxableLineItemVisitor

--- src/Merchello.Core/Gateways/Taxation/TaxableLineItemVisitor.cs
+++ src/Merchello.Core/Gateways/Taxation/TaxableLineItemVisitor.cs
@@ -1,25 +1,34 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-using Merchello.Core.Models;
-
-namespace Merchello.Core.Gateways.Taxation
+﻿namespace Merchello.Core.Gateways.Taxation
 {
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    using Merchello.Core.Models;
+
+    /// <summary>
+    /// A visitor class to identify taxable line items.
+    /// </summary>
     internal class TaxableLineItemVisitor : ILineItemVisitor
     {
+        /// <summary>
+        /// The line items identified as taxable.
+        /// </summary>
         private readonly List<ILineItem> _lineItems = new List<ILineItem>();
+
+        /// <summary>
+        /// The tax rate to be applied to the line item.
+        /// </summary>
         private readonly decimal _taxRate;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaxableLineItemVisitor"/> class.
+        /// </summary>
+        /// <param name="taxRate">
+        /// The tax rate.
+        /// </param>
         public TaxableLineItemVisitor(decimal taxRate)
         {
-            _taxRate = taxRate > 1 ? taxRate/100 : taxRate;
-        }
-
-        public void Visit(ILineItem lineItem)
-        {
-            if (lineItem.ExtendedData.GetTaxableValue())
-            {
-                lineItem.ExtendedData.SetValue(Constants.ExtendedDataKeys.LineItemTaxAmount, (lineItem.TotalPrice * _taxRate).ToString(CultureInfo.InvariantCulture));
-                _lineItems.Add(lineItem);
-            }
+            _taxRate = taxRate > 1 ? taxRate / 100 : taxRate;
         }
 
         /// <summary>
@@ -28,6 +37,20 @@ namespace Merchello.Core.Gateways.Taxation
         public IEnumerable<ILineItem> TaxableLineItems
         {
             get { return _lineItems; }
+        }
+
+        /// <summary>
+        /// The visit.
+        /// </summary>
+        /// <param name="lineItem">
+        /// The line item.
+        /// </param>
+        public void Visit(ILineItem lineItem)
+        {
+            if (!lineItem.ExtendedData.GetTaxableValue()) return;
+            
+            lineItem.ExtendedData.SetValue(Constants.ExtendedDataKeys.LineItemTaxAmount, (lineItem.TotalPrice * this._taxRate).ToString(CultureInfo.InvariantCulture));
+            _lineItems.Add(lineItem);
         }
     }
 }
