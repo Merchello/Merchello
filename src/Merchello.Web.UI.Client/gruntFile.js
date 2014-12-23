085f7bfec00eca2258a51e7d9c446dd4b628bc6b
Rusty Swayne <rusty@mindfly.com>
2014-12-23 10:11:08 -0800

    Adds merchello sass file

--- src/Merchello.Web.UI.Client/gruntFile.js
+++ src/Merchello.Web.UI.Client/gruntFile.js
@@ -1,4 +1,5 @@
 ﻿module.exports = function(grunt) {
+    'use strict';
 
     // Default task.
     //grunt.registerTask('default', ['jshint:dev', 'build', 'karma:unit']);
@@ -15,37 +16,36 @@
     // tried, a few other things but this seems to work the best.
     // https://coderwall.com/p/xnkdqw
     grunt.registerTask('bower', 'Get js packages listed in bower.json',
-        function () {
+        function() {
             var bower = require('bower');
             var done = this.async();
 
             bower.commands.install(undefined, {}, { interactive: false })
-                .on('log', function (data) {
+                .on('log', function(data) {
                     grunt.log.write(data.message + "\n");
                 })
-                .on('error', function (data) {
+                .on('error', function(data) {
                     grunt.log.write(data.message + "\n");
                     done(false);
                 })
-                .on('end', function (data) {
+                .on('end', function(data) {
                     done();
                 });
-        }
-      );
+        });
 
     // Project configuration.
     grunt.initConfig({
         buildVersion: grunt.option('buildversion') || '1',
-        distdir: 'build/merchello',
+        distdir: 'build/App_Plugins/Merchello',
         bowerfiles: 'bower_components',
         vsdir: '../Merchello.Web.UI/App_Plugins/Merchello2',
         pkg: grunt.file.readJSON('package.json'),
 
         banner:
             '/*! <%= pkg.title || pkg.name %>\n' +
-                '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
-                ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;\n' +
-                ' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */\n',
+            '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+            ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;\n' +
+            ' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */\n',
 
         src: {
             js: ['src/**/*.js', 'src/*.js'],
@@ -79,16 +79,16 @@
             mocks: {
                 files: [{ dest: '<%= distdir %>/js', src: '*.js', expand: true, cwd: 'src/common/mocks/' }]
             },
-            
-            manifest: {
-                files: [{ dest: '<%= distdir %>/', src: '*.manifest', expand: false, cwd: 'src/' }]  
-            },
+
+            //manifest: {
+            //    files: [{ dest: '<%= distdir %>/', src: '*.manifest', expand: false, cwd: 'src/' }]
+            //},
 
             vs: {
                 files: [
                     //everything except the index.html root file!
                     //then we need to figure out how to not copy all the test stuff either!?
-                    { dest: '<%= vsdir %>', src: '*.manfiest', expad: false, cwd: '<%= distdir %>'},
+                    { dest: '<%= vsdir %>', src: '*.manfiest', expad: false, cwd: '<%= distdir %>' },
                     { dest: '<%= vsdir %>/assets', src: '**', expand: true, cwd: '<%= distdir %>/assets' },
                     { dest: '<%= vsdir %>/js', src: '**', expand: true, cwd: '<%= distdir %>/js' },
                     { dest: '<%= vsdir %>/lib', src: '**', expand: true, cwd: '<%= distdir %>/lib' },
@@ -162,63 +162,62 @@
         //        watch: { configFile: 'test/config/unit.js', singleRun: false, autoWatch: true, keepalive: true }
         //},
 
-        jshint: {
-            dev: {
-                files: {
-                    src: ['<%= src.common %>', '<%= src.specs %>', '<%= src.scenarios %>']
-                },
-                options: {
-                    curly: true,
-                    eqeqeq: true,
-                    immed: true,
-                    latedef: true,
-                    newcap: true,
-                    noarg: true,
-                    sub: true,
-                    boss: true,
-                    //NOTE: This is required so it doesn't barf on reserved words like delete when doing $http.delete
-                    es5: true,
-                    eqnull: true,
-                    //NOTE: we need to use eval sometimes so ignore it
-                    evil: true,
-                    //NOTE: we need to check for strings such as "javascript:" so don't throw errors regarding those
-                    scripturl: true,
-                    //NOTE: we ignore tabs vs spaces because enforcing that causes lots of errors depending on the text editor being used
-                    smarttabs: true,
-                    globals: {}
-                }
-            },
+        //jshint: {
+        //    dev: {
+        //        files: {
+        //            src: ['<%= src.common %>', '<%= src.specs %>', '<%= src.scenarios %>']
+        //        },
+        //        options: {
+        //            curly: true,
+        //            eqeqeq: true,
+        //            immed: true,
+        //            latedef: true,
+        //            newcap: true,
+        //            noarg: true,
+        //            sub: true,
+        //            boss: true,
+        //            //NOTE: This is required so it doesn't barf on reserved words like delete when doing $http.delete
+        //            es5: true,
+        //            eqnull: true,
+        //            //NOTE: we need to use eval sometimes so ignore it
+        //            evil: true,
+        //            //NOTE: we need to check for strings such as "javascript:" so don't throw errors regarding those
+        //            scripturl: true,
+        //            //NOTE: we ignore tabs vs spaces because enforcing that causes lots of errors depending on the text editor being used
+        //            smarttabs: true,
+        //            globals: {}
+        //        }
+        //    },
 
-            // merchello plugins
-            build: {
-                files: {
-                    src: ['<%= src.prod %>']
-                },
-                options: {
-                    curly: true,
-                    eqeqeq: true,
-                    immed: true,
-                    latedef: true,
-                    newcap: true,
-                    noarg: true,
-                    sub: true,
-                    boss: true,
-                    //NOTE: This is required so it doesn't barf on reserved words like delete when doing $http.delete
-                    es5: true,
-                    eqnull: true,
-                    //NOTE: we need to use eval sometimes so ignore it
-                    evil: true,
-                    //NOTE: we need to check for strings such as "javascript:" so don't throw errors regarding those
-                    scripturl: true,
-                    //NOTE: we ignore tabs vs spaces because enforcing that causes lots of errors depending on the text editor being used
-                    smarttabs: true,
-                    globalstrict: true,
-                    globals: { $: false, jQuery: false, define: false, require: false, window: false }
-                }
-            }
-        }
+        //    // merchello plugins
+        //    build: {
+        //        files: {
+        //            src: ['<%= src.prod %>']
+        //        },
+        //        options: {
+        //            curly: true,
+        //            eqeqeq: true,
+        //            immed: true,
+        //            latedef: true,
+        //            newcap: true,
+        //            noarg: true,
+        //            sub: true,
+        //            boss: true,
+        //            //NOTE: This is required so it doesn't barf on reserved words like delete when doing $http.delete
+        //            es5: true,
+        //            eqnull: true,
+        //            //NOTE: we need to use eval sometimes so ignore it
+        //            evil: true,
+        //            //NOTE: we need to check for strings such as "javascript:" so don't throw errors regarding those
+        //            scripturl: true,
+        //            //NOTE: we ignore tabs vs spaces because enforcing that causes lots of errors depending on the text editor being used
+        //            smarttabs: true,
+        //            globalstrict: true,
+        //            globals: { $: false, jQuery: false, define: false, require: false, window: false }
+        //        }
+        //    }
+        //}
     });
-
 
 
     grunt.loadNpmTasks('grunt-contrib-concat');
@@ -226,8 +225,9 @@
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-   // grunt.loadNpmTasks('grunt-contrib-watch');
-   // grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-recess');
 
     grunt.loadNpmTasks('grunt-karma');
 
