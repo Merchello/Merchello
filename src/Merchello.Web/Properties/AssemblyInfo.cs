c9e3a243366cfe07b4be9bbad9313ca1e0844542
Rusty Swayne <rusty@mindfly.com>
2015-01-31 09:55:29 -0800

    Patches build script.

--- src/Merchello.Web/Properties/AssemblyInfo.cs
+++ src/Merchello.Web/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.6.0.3")]
-[assembly: AssemblyFileVersion("1.6.0.3")]
-[assembly: AssemblyInformationalVersion("1.6.0.3")]
+[assembly: AssemblyVersion("1.7.0.1")]
+[assembly: AssemblyFileVersion("1.7.0.1")]
+[assembly: AssemblyInformationalVersion("1.7.0.1")]
 
 
