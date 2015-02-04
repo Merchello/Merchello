2e58b831fdf17b89704135e531fd959f7a2403e1
Rusty Swayne <rusty@mindfly.com>
2015-02-04 15:08:37 -0800

    Final tweaks to braintree build script

--- Plugin/Payments/BrainTree/src/Merchello.Plugin.Payments.BrainTree/Properties/VersionInfo.cs
+++ Plugin/Payments/BrainTree/src/Merchello.Plugin.Payments.BrainTree/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.309.40866")]
+[assembly: AssemblyFileVersion("2.0.309.41280")]
 [assembly: AssemblyInformationalVersion("2.0.0.0")]
 
 
