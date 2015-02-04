b316898ef57c987a559712bb6406e9689abcf7e1
Rusty Swayne <rusty@mindfly.com>
2015-02-03 16:42:17 -0800

    Added sales reports to HBOX. TODO, currency code needs to be respected and displayed correctly.

--- Plugin/Reports/SalesReports/src/Merchello.Reports.SalesReports/Properties/VersionInfo.cs
+++ Plugin/Reports/SalesReports/src/Merchello.Reports.SalesReports/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.94.37833")]
+[assembly: AssemblyFileVersion("2.0.94.45188")]
 [assembly: AssemblyInformationalVersion("2.0.0.0")]
 
 
