d24a494f04c947255b2a000620b9fb8f1faf73ac
Rusty Swayne <rusty@mindfly.com>
2015-02-25 08:54:01 -0800

    [WIP] Merchello.Bazaar installer.

---
+++ src/Merchello.Bazaar/Install/BazaarDataInstaller.cs
@@ -0,0 +1,23 @@
+﻿namespace Merchello.Bazaar.Install
+{
+    using System;
+
+    using Umbraco.Core.Models;
+
+    /// <summary>
+    /// The bazaar data installer.
+    /// </summary>
+    public class BazaarDataInstaller
+    {
+        /// <summary>
+        /// The execute.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IContent"/>.
+        /// </returns>
+        public IContent Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Merchello.Bazaar/Install/Installer.cs
@@ -0,0 +1,23 @@
+﻿namespace Merchello.Bazaar.Install
+{
+    using System;
+
+    using Umbraco.Web.UI.Controls;
+
+    /// <summary>
+    /// The installer.
+    /// </summary>
+    public class Installer : UmbracoUserControl
+    {
+        /// <summary>
+        /// The on init.
+        /// </summary>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+        }
+    }
+}
