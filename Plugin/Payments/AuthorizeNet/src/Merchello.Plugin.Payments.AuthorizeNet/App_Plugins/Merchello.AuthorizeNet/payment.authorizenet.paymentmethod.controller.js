5fd18c4147db2dac824d4e25a748dde2d91bdc75
Jason Prothero <protherj@gmail.com>
2015-03-06 12:59:15 -0800

    Upgrade/refactor for 1.8.0

---
+++ Plugin/Payments/AuthorizeNet/src/Merchello.Plugin.Payments.AuthorizeNet/App_Plugins/Merchello.AuthorizeNet/payment.authorizenet.paymentmethod.controller.js
@@ -0,0 +1,13 @@
+﻿/**
+ * @ngdoc controller
+ * @name Merchello.Dashboards.Settings.Payment.Dialogs.PaymentMethodController
+ * @function
+ * 
+ * @description
+ * The controller for the adding / editing payment methods on the Payment page
+ */
+angular.module('merchello.plugins.authorizenet').controller('Merchello.Plugin.GatewayProviders.Payments.Dialogs.AuthorizeNetPaymentMethodController',
+    ['$scope',
+        function ($scope) {
+
+        }]);
