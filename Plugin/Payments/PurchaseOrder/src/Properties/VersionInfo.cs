919b6408ba645f979f35a175dfb19a398bb764b3
Jason Prothero <protherj@gmail.com>
2015-03-06 09:39:45 -0800

    build Umbraco Package and update readmes

--- Plugin/Payments/PurchaseOrder/src/Properties/VersionInfo.cs
+++ Plugin/Payments/PurchaseOrder/src/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.339.23715")]
+[assembly: AssemblyFileVersion("1.0.339.26097")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
