41a57ce2c516d46ed6c1da3312455c16f96147b7
Rusty Swayne <rusty@mindfly.com>
2015-03-18 14:37:54 -0700

    Adds CampaignSettingsService to the IServiceContext

---
+++ test/Merchello.Tests.IntegrationTests/Services/CampaignSettings/CampaignSettingsServiceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Merchello.Tests.IntegrationTests.Services.CampaignSettings
+{
+    using Merchello.Core;
+    using Merchello.Core.Services;
+    using Merchello.Tests.Base.TestHelpers;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CampaignSettingsServiceTests : MerchelloAllInTestBase
+    {
+        private ICampaignSettingsService _campaignSettingsService;
+
+        public void Init()
+        {
+            _campaignSettingsService = MerchelloContext.Current.Services.CampaignSettingsService;
+        }
+    }
+}
