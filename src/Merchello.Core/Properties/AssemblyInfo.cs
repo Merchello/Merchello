0c956395f0951090119bc541f1372923807d4c11
Rusty Swayne <rusty@mindfly.com>
2015-05-06 15:29:40 -0700

    Test Bazaar on 1.8.3

--- src/Merchello.Core/Properties/AssemblyInfo.cs
+++ src/Merchello.Core/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.8.2.11")]
-[assembly: AssemblyFileVersion("1.8.2.11")]
-[assembly: AssemblyInformationalVersion("1.8.2.11")]
+[assembly: AssemblyVersion("1.8.2.16")]
+[assembly: AssemblyFileVersion("1.8.2.16")]
+[assembly: AssemblyInformationalVersion("1.8.2.16")]
 
 
