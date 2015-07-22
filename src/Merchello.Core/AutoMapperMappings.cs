60fc46d5c832adb3f8f984b25f647ac097215e2b
Rusty Swayne <rs@merchello.com>
2015-07-22 10:23:39 -0700

    More mappings

---
+++ src/Merchello.Core/AutoMapperMappings.cs
@@ -0,0 +1,20 @@
+﻿namespace Merchello.Core
+{
+    using Merchello.Core.Gateways.Notification.Monitors;
+    using Merchello.Core.Models;
+
+    /// <summary>
+    /// The auto mapper mappings.
+    /// </summary>
+    internal static class AutoMapperMappings
+    {
+        /// <summary>
+        /// The create mappings.
+        /// </summary>
+        public static void CreateMappings()
+        {
+            // Notification message
+            AutoMapper.Mapper.CreateMap<INotificationMessage, NotificationMonitorMessage>();
+        }    
+    }
+}
