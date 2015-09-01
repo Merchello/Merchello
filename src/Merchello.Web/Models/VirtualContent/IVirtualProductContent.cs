be5e40c9b9f9f08e5c80b01e4d1086ad8941171e
Rusty Swayne <rs@merchello.com>
2015-09-01 13:47:25 -0700

    Initial work on VirtualProductContent

---
+++ src/Merchello.Web/Models/VirtualContent/IVirtualProductContent.cs
@@ -0,0 +1,18 @@
+﻿namespace Merchello.Web.Models.DetachedContent
+{
+    using System;
+
+    using Merchello.Core.Models;
+
+    using Umbraco.Core.Models;
+
+    /// <summary>
+    /// Defines a virtual product content.
+    /// </summary>
+    public interface IVirtualProductContent : IPublishedContent
+    {
+        Guid Key { get; }
+
+        string CultureName { get; }
+    }
+}
