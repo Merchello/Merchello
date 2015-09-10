6c43f9bae953e606563776cda04e4bad540faf77
Rusty Swayne <rs@merchello.com>
2015-09-10 15:48:49 -0700

    [Bazaar] Updated installer

--- src/Merchello.Core/Properties/AssemblyInfo.cs
+++ src/Merchello.Core/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.11.0.13")]
-[assembly: AssemblyFileVersion("1.11.0.13")]
-[assembly: AssemblyInformationalVersion("1.11.0.13")]
+[assembly: AssemblyVersion("1.11.0.15")]
+[assembly: AssemblyFileVersion("1.11.0.15")]
+[assembly: AssemblyInformationalVersion("1.11.0.15")]
 
 
