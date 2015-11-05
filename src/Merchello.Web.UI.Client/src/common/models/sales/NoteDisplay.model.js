7b1a4ca18f37272d14e89912c05727bc057ab4f7
Rusty Swayne <rs@merchello.com>
2015-11-05 12:20:10 -0800

    Splits Notes angular module and builder factory into separate files.

--- src/Merchello.Web.UI.Client/src/common/models/sales/NoteDisplay.model.js
+++ src/Merchello.Web.UI.Client/src/common/models/sales/NoteDisplay.model.js
@@ -37,30 +37,3 @@ NoteDisplay.prototype = (function () {
 
 angular.module('merchello.models').constant('NoteDisplay', NoteDisplay);
 
-/**
-* @ngdoc service
-* @name merchello.models.noteDisplayBuilder
-*
-* @description
-* A utility service that builds noteDisplayBuilder models
-*/
-angular.module('merchello.models')
-   .factory('noteDisplayBuilder',
-       ['genericModelBuilder', 'NoteDisplay',
-       function (genericModelBuilder, NoteDisplay) {
-
-           var Constructor = NoteDisplay;
-
-           return {
-               createDefault: function () {
-                   return new Constructor();
-               },
-               transform: function (jsonResult) {
-                   var noteDisplay = genericModelBuilder.transform(jsonResult, Constructor);
-
-                   noteDisplay.message = jsonResult.message;
-
-                   return noteDisplay;
-               }
-           };
-       }]);
