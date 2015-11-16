b4ceabbc04939b7ce5f2150f96c0dbe374f6c056
Rusty Swayne <rs@merchello.com>
2015-11-16 09:27:14 -0800

    Completes M-908

--- src/Merchello.Web/Workflow/CustomerItemCache/CustomerItemCacheBase.cs
+++ src/Merchello.Web/Workflow/CustomerItemCache/CustomerItemCacheBase.cs
@@ -90,6 +90,16 @@
         public event TypedEventHandler<CustomerItemCacheBase, Core.Events.NewEventArgs<ILineItem>> AddedItem;
 
         /// <summary>
+        /// Occurs before an item quatity is updated.
+        /// </summary>
+        public event TypedEventHandler<CustomerItemCacheBase, Core.Events.UpdateItemEventArgs<ILineItem>> UpdatingItem;
+
+        /// <summary>
+        /// Occurs after an item quatity is updated.
+        /// </summary>
+        public event TypedEventHandler<CustomerItemCacheBase, Core.Events.UpdateItemEventArgs<ILineItem>> UpdatedItem;
+
+        /// <summary>
         /// Occurs before removing an item.
         /// </summary>
         public event TypedEventHandler<CustomerItemCacheBase, DeleteEventArgs<ILineItem>> RemovingItem;
@@ -612,7 +622,11 @@
                 return;
             }
 
+            UpdatingItem.RaiseEvent(new UpdateItemEventArgs<ILineItem>(_itemCache.Items[sku]), this);
+
             _itemCache.Items[sku].Quantity = quantity;
+
+            UpdatedItem.RaiseEvent(new UpdateItemEventArgs<ILineItem>(_itemCache.Items[sku]), this);
         }
 
         /// <summary>
