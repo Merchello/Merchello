633b2c369db9900c0dd6bf7962256e491345e0bc
Rusty Swayne <rs@merchello.com>
2015-12-23 10:42:10 -0800

    Sets assembly visibility.

--- src/Merchello.Core/Properties/AssemblyVisibility.cs
+++ src/Merchello.Core/Properties/AssemblyVisibility.cs
@@ -1,5 +1,6 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Examine")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Web")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.UaaS")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.Base")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.UnitTests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Merchello.Tests.IntegrationTests")]
