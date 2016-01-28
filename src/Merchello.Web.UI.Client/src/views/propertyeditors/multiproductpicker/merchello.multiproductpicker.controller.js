0e327851fb9bca232bd1709ed35e8dca944d7d06
Rusty Swayne <rs@merchello.com>
2016-01-28 10:34:19 -0800

    Fixes styling of bazaar sale pricing

--- src/Merchello.Web.UI.Client/src/views/propertyeditors/multiproductpicker/merchello.multiproductpicker.controller.js
+++ src/Merchello.Web.UI.Client/src/views/propertyeditors/multiproductpicker/merchello.multiproductpicker.controller.js
@@ -1,14 +1,52 @@
 angular.module('merchello').controller('Merchello.PropertyEditors.MerchelloMultiProductPickerController',
-    ['$scope', 'dialogService',
-    function($scope, dialogService) {
+    ['$scope', '$q', 'dialogService', 'settingsResource', 'productResource', 'productDisplayBuilder',
+    function($scope, $q, dialogService, settingsResource, productResource, productDisplayBuilder) {
 
         $scope.preValuesLoaded = false;
+        $scope.keys = [];
         $scope.products = [];
+        $scope.settings = {};
         $scope.remove = remove;
         $scope.openPickerDialog = openPickerDialog;
 
+        $scope.sortProperty = '';
+        $scope.sortOrder = 'Ascending';
+        $scope.filterText = '';
+        $scope.limitAmount = 5;
+        $scope.currentPage = 0;
+        $scope.maxPages = 0;
 
         function init() {
+            if ($scope.model.value !== '') {
+                $scope.keys = JSON.parse($scope.model.value);
+            }
+
+            if ($scope.keys.length > 0) {
+                $q.all([
+                    settingsResource.getAllSettings(),
+                    productResource.getByKeys($scope.keys)
+                ]).then(function(data) {
+                    $scope.settings = data[0].settings;
+                    console.info($scope.settings);
+                });
+            }
+        }
+
+        /**
+         * @ngdoc method
+         * @name loadSettings
+         * @function
+         *
+         * @description
+         * Load the settings from the settings service to get the currency symbol
+         */
+        function loadSettings() {
+            // this is needed for the date format
+            var settingsPromise = settingsResource.getAllSettings();
+
+        }
+
+        function loadProducts() {
 
         }
 
@@ -18,9 +56,20 @@ angular.module('merchello').controller('Merchello.PropertyEditors.MerchelloMulti
         }
 
         function openPickerDialog() {
+            var dialogData = {};
+            dialogData.products = $scope.products;
+            dialogService.open({
+                template: '/App_Plugins/Merchello/propertyeditors/multiproductpicker/merchello.multiproductpicker.dialog.html',
+                show: true,
+                callback: selectProductFromDialog,
+                dialogData: dialogData
+            });
+        }
+
+        function selectProductFromDialog(dialogData) {
 
         }
 
         init();
-        
+
     }]);
---
+++ src/Merchello.Web.UI.Client/src/views/propertyeditors/multiproductpicker/merchello.multiproductpicker.dialog.controller.js
@@ -0,0 +1,18 @@
+angular.module('merchello').controller('Merchello.PropertyEditors.MerchelloMultiProductPickerDialogController',
+    ['$scope', 'productResource',
+    function($scope, productResource) {
+
+        $scope.loaded = true;
+
+
+        function init() {
+
+        }
+
+        function loadProducts() {
+
+        }
+
+        init();
+
+    }]);
