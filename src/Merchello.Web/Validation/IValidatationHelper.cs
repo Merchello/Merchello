73635c80d20dd2936b232f47f6743c8b057d3e8d
rs@merchello.com <rs@merchello.com>
2016-04-15 07:56:44 -0700

    M-1002 Completed

--- src/Merchello.Web/Validation/IValidatationHelper.cs
+++ src/Merchello.Web/Validation/IValidatationHelper.cs
@@ -1,5 +1,7 @@
 ﻿namespace Merchello.Web.Validation
 {
+    using Merchello.Core.Validation;
+
     /// <summary>
     /// Defines a ValidationHelper.
     /// </summary>
@@ -9,5 +11,10 @@
         /// Gets the <see cref="IBankingValidationHelper"/>.
         /// </summary>
         IBankingValidationHelper Banking { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IEmailValidationHelper"/>.
+        /// </summary>
+        IEmailValidationHelper Email { get; }
     }
 }
--- src/Merchello.Web/Validation/ValidationHelper.cs
+++ src/Merchello.Web/Validation/ValidationHelper.cs
@@ -2,6 +2,8 @@
 {
     using System;
 
+    using Merchello.Core.Validation;
+
     /// <summary>
     /// Represents a ValidationHelper.
     /// </summary>
@@ -13,6 +15,11 @@
         private readonly Lazy<IBankingValidationHelper> _banking = new Lazy<IBankingValidationHelper>(() => new BankingValidationHelper());
 
         /// <summary>
+        /// The <see cref="IEmailValidationHelper"/>.
+        /// </summary>
+        private readonly Lazy<IEmailValidationHelper> _email = new Lazy<IEmailValidationHelper>(() => new EmailValidationHelper());
+
+        /// <summary>
         /// Gets the <see cref="IBankingValidationHelper"/>.
         /// </summary>
         public IBankingValidationHelper Banking
@@ -20,6 +27,17 @@
             get
             {
                 return _banking.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IEmailValidationHelper"/>.
+        /// </summary>
+        public IEmailValidationHelper Email
+        {
+            get
+            {
+                return _email.Value;
             }
         }
     }
