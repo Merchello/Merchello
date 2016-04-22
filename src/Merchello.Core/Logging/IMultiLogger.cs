4f9110fcaec5cb2f04fc7a9d17e92da006a09eb2
rs@merchello.com <rs@merchello.com>
2016-04-22 14:34:36 -0700

    Exposes UmbracoLogger and RemoteLogger on MultiLogHelper as properties.

--- src/Merchello.Core/Logging/IMultiLogger.cs
+++ src/Merchello.Core/Logging/IMultiLogger.cs
@@ -7,5 +7,14 @@
     /// </summary>
     public interface IMultiLogger : ILogger, IExtendedLoggerDataLogger
     {
+        /// <summary>
+        /// Gets the Umbraco <see cref="ILogger"/>.
+        /// </summary>
+        ILogger UmbracoLogger { get; }
+
+        /// <summary>
+        /// Gets the remote logger.
+        /// </summary>
+        IRemoteLogger RemoteLogger { get; }
     }
 }
