cefbe734e5041c85c21c07c48e45df391c547eda
Trevor Loader <trevor@webconception.co.nz>
2016-08-24 06:19:27 +1200

    Add change password and forgot password functionality to FastTrack

---
+++ src/Merchello.FastTrack/Models/Membership/ChangePasswordModel.cs
@@ -0,0 +1,37 @@
+﻿namespace Merchello.FastTrack.Models.Membership
+{
+	using System.ComponentModel.DataAnnotations;
+
+	using Merchello.Web.Store.Localization;
+
+	/// <summary>
+	/// A model used for authentication.
+	/// </summary>
+	/// <remarks>
+	/// Could probably just use Umbraco's LoginModel here 
+	/// </remarks>
+	public class ChangePasswordModel
+	{
+		/// <summary>
+		/// Gets or sets the old password.
+		/// </summary>
+		[Required(ErrorMessageResourceType = typeof(StoreFormsResource), ErrorMessageResourceName = "RequiredOldPassword")]
+		[Display(ResourceType = typeof(StoreFormsResource), Name = "LabelOldPassword"), DataType(DataType.Password)]
+		public string OldPassword { get; set; }
+
+		/// <summary>
+		/// Gets or sets the new password.
+		/// </summary>
+		[Required(ErrorMessageResourceType = typeof(StoreFormsResource), ErrorMessageResourceName = "RequiredPassword")]
+		[Display(ResourceType = typeof(StoreFormsResource), Name = "LabelPassword"), DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		/// <summary>
+		/// Gets or sets the confirm password.
+		/// </summary>
+		[Required(ErrorMessageResourceType = typeof(StoreFormsResource), ErrorMessageResourceName = "RequiredConfirmPassword")]
+		[Display(ResourceType = typeof(StoreFormsResource), Name = "LabelConfirmPassword"), DataType(DataType.Password)]
+		[Compare("Password", ErrorMessageResourceType = typeof(StoreFormsResource), ErrorMessageResourceName = "InvalidConfirmPassword")]
+		public string ConfirmPassword { get; set; }
+	}
+}
