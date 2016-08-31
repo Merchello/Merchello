614308eb9342f12ff5332b5b7c6aac300e8ba473
Lee Messenger <lee@aptitude.co.uk>
2016-08-31 07:58:27 +0100

    Updated FastTrack to use config to help with customisation/integration
    Removed semi colon in View
    Added new IEnumerable/Array extention to help with rendering products in view
    Changed 'Save' to 'Next' in the checkout

--- src/Merchello.Core/EnumerableExtensions.cs
+++ src/Merchello.Core/EnumerableExtensions.cs
@@ -29,6 +29,21 @@
                     {
                         item
                     }));
-        }        
+        }
+
+        /// <summary>
+        /// Splits an array into several smaller arrays.
+        /// </summary>
+        /// <typeparam name="T">The type of the array.</typeparam>
+        /// <param name="array">The array to split.</param>
+        /// <param name="size">The size of the smaller arrays.</param>
+        /// <returns>An array containing smaller arrays.</returns>
+        public static IEnumerable<IEnumerable<T>> Split<T>(this T[] array, int size)
+        {
+            for (var i = 0; i < (float)array.Length / size; i++)
+            {
+                yield return array.Skip(i * size).Take(size);
+            }
+        }
     }
 }
