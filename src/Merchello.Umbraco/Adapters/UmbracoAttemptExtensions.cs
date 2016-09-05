ccc453e554ffb6f67ad968b413a1cfecfc079520
rs@merchello.com <rs@merchello.com>
2016-09-04 17:55:54 -0700

    [WIP] Umbraco adapters

---
+++ src/Merchello.Umbraco/Adapters/UmbracoAttemptExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Merchello.Umbraco.Adapters
+{
+    using Merchello.Core;
+
+    internal static class UmbracoAttemptExtensions
+    {
+        public static global::Umbraco.Core.Attempt<T> FU<T>(this Attempt<T> merchAttempt)
+        {
+            return merchAttempt.ForUmbraco<T>();
+        }
+
+        public static global::Umbraco.Core.Attempt<T> ForUmbraco<T>(this Attempt<T> merchAttempt)
+        {
+            // TODO needs to be tested!!!
+            return merchAttempt.Success
+                       ? global::Umbraco.Core.Attempt<T>.Succeed(merchAttempt.Result)
+                       : global::Umbraco.Core.Attempt<T>.Fail(merchAttempt.Result, merchAttempt.Exception);
+        }
+    }
+}
