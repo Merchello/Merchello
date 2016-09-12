5afa8422ba5c53c4e59c06474029955dcfee438d
rs@merchello.com <rs@merchello.com>
2016-09-12 12:16:14 -0700

    Base resolvers

---
+++ src/Merchello.Core/Logging/MultiLogResolver.cs
@@ -0,0 +1,40 @@
+﻿namespace Merchello.Core.Logging
+{
+
+    /// <summary>
+    /// The multi logger resolver.
+    /// </summary>
+    internal sealed class MultiLogResolver : SingleObjectResolverBase<MultiLogResolver, IMultiLogger>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiLogResolver"/> class.
+        /// </summary>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        public MultiLogResolver(IMultiLogger logger)
+            : base(logger)
+        {            
+        }
+
+        /// <summary>
+        /// Gets the current logger
+        /// </summary>
+        public IMultiLogger Logger
+        {
+            get { return Value; }
+        }
+
+        /// <summary>
+        /// Method allowing to change the logger during startup
+        /// </summary>
+        /// <param name="logger">
+        /// The <see cref="IMultiLogger"/>
+        /// </param>
+        internal void SetLogger(IMultiLogger logger)
+        {
+            Value = logger;
+        }
+
+    }
+}
