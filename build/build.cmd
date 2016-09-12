3b4ef4c826886e531245e148bab9e7e40d17cb2e
rs@merchello.com <rs@merchello.com>
2016-09-12 11:34:21 -0700

    [WIP] MSBuild.  Merchello.FastTrack.Ui

--- build/build.cmd
+++ build/build.cmd
@@ -1,32 +1,139 @@
-ECHO off
+@ECHO OFF
 
-SET /P APPVEYOR_BUILD_NUMBER=Please enter a build number (e.g. 134):
-SET /P PACKAGE_VERSION=Please enter your package version (e.g. 1.0.5):
-SET /P UMBRACO_PACKAGE_PRERELEASE_SUFFIX=Please enter your package release suffix or leave empty (e.g. beta):
+:: MERCHELLO BUILD FILE
 
-SET /P APPVEYOR_REPO_TAG=If you want to simulate a GitHub tag for a release (e.g. true):
+ECHO.
 
-if "%APPVEYOR_BUILD_NUMBER%" == "" (
-  SET APPVEYOR_BUILD_NUMBER=100
-)
-if "%PACKAGE_VERSION%" == "" (
-  SET PACKAGE_VERSION=0.1.0
+:: ensure we have MerchelloVersion.txt
+IF NOT EXIST MerchelloVersion.txt (
+	ECHO MerchelloVersion.txt is missing!
+	GOTO error
 )
 
-SET APPVEYOR_BUILD_VERSION=%PACKAGE_VERSION%.%APPVEYOR_BUILD_NUMBER%
+:: get the version and comment from MerchelloVersion.txt lines 2 and 3
+SET RELEASE=
+SET COMMENT=
+FOR /F "skip=1 delims=" %%i IN (MerchelloVersion.txt) DO IF NOT DEFINED RELEASE SET RELEASE=%%i
+FOR /F "skip=2 delims=" %%i IN (MerchelloVersion.txt) DO IF NOT DEFINED COMMENT SET COMMENT=%%i
 
-if "%APPVEYOR_REPO_TAG%" == "true" (
-	SET MERCHELLO_VERSION = %PACKAGE_VERSION%
-) else (
-	SET MERCHELLO_VERSION = %APPVEYOR_BUILD_VERSION%
-)
+:: process args
 
-:: CALL build-grunt.cmd
+SET INTEGRATION=0
+::SET nuGetFolder=%CD%\..\src\packages\
+SET nuGetFolder=..\src\packages\
+SET SKIPNUGET=0
 
-build-appveyor.cmd
 
-@IF %ERRORLEVEL% NEQ 0 GOTO err
-@EXIT /B 0
-:err
-@PAUSE
-@EXIT /B 1
+:processArgs
+
+:: grab the first parameter as a whole eg "/action:start"
+:: end if no more parameter
+SET SWITCHPARSE=%1
+IF [%SWITCHPARSE%] == [] goto endProcessArgs
+
+:: get switch and value
+SET SWITCH=
+SET VALUE=
+FOR /F "tokens=1,* delims=: " %%a IN ("%SWITCHPARSE%") DO SET SWITCH=%%a& SET VALUE=%%b
+
+:: route arg
+IF '%SWITCH%'=='/release' GOTO argRelease
+IF '%SWITCH%'=='-release' GOTO argRelease
+IF '%SWITCH%'=='/comment' GOTO argComment
+IF '%SWITCH%'=='-comment' GOTO argComment          
+IF '%SWITCH%'=='/integration' GOTO argIntegration  
+IF '%SWITCH%'=='-integration' GOTO argIntegration 
+IF '%SWITCH%'=='/nugetfolder' GOTO argNugetFolder
+IF '%SWITCH%'=='-nugetfolder' GOTO argNugetFolder
+IF '%SWITCH%'=='/skipnuget' GOTO argSkipNuget
+IF '%SWITCH%'=='-skipnuget' GOTO argSkipNuget
+ECHO "Invalid switch %SWITCH%"
+GOTO error
+
+:: handle each arg
+
+:argRelease
+set RELEASE=%VALUE%
+SHIFT
+goto processArgs
+
+:argComment
+SET COMMENT=%VALUE%
+SHIFT
+GOTO processArgs
+
+:argIntegration
+SET INTEGRATION=1
+SHIFT
+GOTO processArgs
+
+:argNugetFolder
+SET nuGetFolder=%VALUE%
+SHIFT
+GOTO processArgs
+
+:argSkipNuget
+SET SKIPNUGET=1
+SHIFT
+GOTO processArgs
+
+:endProcessArgs 
+
+:: run
+
+
+SET VERSION=%RELEASE%
+IF [%COMMENT%] EQU [] (SET VERSION=%RELEASE%) ELSE (SET VERSION=%RELEASE%-%COMMENT%)
+
+ECHO.
+ECHO Building Merchello %VERSION%
+ECHO.
+
+
+SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe"
+SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
+
+:: FastTrack
+:: ReplaceIISExpressPortNumber.exe ..\src\Merchello.FastTrack.UI\Merchello.FastTrack.UI.csproj %RELEASE%
+
+ECHO.
+ECHO Removing the UI build folder and bower_components folder to make sure everything is clean as a whistle
+RD ..\src\Merchello.Umbraco.UI.Client\build /Q /S
+:: RD ..\src\Merchello.Umbraco.UI.Client\bower_components /Q /S
+
+
+ECHO.
+ECHO Restoring NuGet packages
+ECHO Into %nuGetFolder%
+..\src\.nuget\NuGet.exe restore ..\src\Merchello.Core\project.json -OutputDirectory %nuGetFolder% -Verbosity quiet
+..\src\.nuget\NuGet.exe restore ..\src\Merchello.Web\project.json -OutputDirectory %nuGetFolder% -Verbosity quiet
+..\src\.nuget\NuGet.exe restore ..\src\Merchello.Providers\project.json -OutputDirectory %nuGetFolder% -Verbosity quiet
+..\src\.nuget\NuGet.exe restore ..\src\Merchello.Umbraco\project.json -OutputDirectory %nuGetFolder% -Verbosity quiet
+
+ECHO.
+ECHO Performing MSBuild and producing Merchello binaries zip files
+ECHO This takes a few minutes and logging is set to report warnings
+ECHO and errors only so it might seems like nothing is happening for a while. 
+ECHO You can check the msbuild.log file for progress.
+ECHO.
+%MSBUILD% "Merchello.proj" /p:BUILD_RELEASE=%RELEASE% /p:BUILD_COMMENT=%COMMENT% /p:NugetPackagesDirectory=%nuGetFolder% /consoleloggerparameters:Summary;ErrorsOnly;WarningsOnly /fileLogger
+IF ERRORLEVEL 1 GOTO error
+
+
+:success
+ECHO.
+ECHO No errors were detected!
+ECHO There may still be some in the output, which you would need to investigate.
+ECHO Warnings are usually normal.
+ECHO.
+GOTO :EOF
+
+:error
+
+ECHO.
+ECHO Errors were detected!
+ECHO.
+
+:: don't pause if continuous integration else the build server waits forever
+:: before cancelling the build (and, there is noone to read the output anyways)
+IF %INTEGRATION% NEQ 1 PAUSE
---
+++ src/SolutionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("Across the Pond")]
+[assembly: AssemblyCopyright("Copyright � Across the Pond 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-alpha0000")]
