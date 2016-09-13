3aa3d3db3c42df809f34c3a86c618e3d183d3936
rs@merchello.com <rs@merchello.com>
2016-09-12 19:38:46 -0700

    LineItemDto

---
+++ src/Merchello.Core/Models/Rdbms/LineItemDto.cs
@@ -0,0 +1,45 @@
+﻿namespace Merchello.Core.Models.Rdbms
+{
+    using System;
+
+    using Merchello.Core.Acquired.Persistence.DatabaseAnnotations;
+
+    using NPoco;
+
+    /// <inheritdoc/>
+    internal abstract class LineItemDto : EntityDto, ILineItemDto
+    {
+        /// <inheritdoc/>
+        [Column("extendedData")]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        [SpecialDbType(SpecialDbTypes.NTEXT)]
+        public string ExtendedData { get; set; }
+
+        /// <inheritdoc/>
+        public abstract Guid ContainerKey { get; set; }
+
+        /// <inheritdoc/>
+        [Column("lineItemTfKey")]
+        public virtual Guid LineItemTfKey { get; set; }
+
+        /// <inheritdoc/>
+        [Column("sku")]
+        public virtual string Sku { get; set; }
+
+        /// <inheritdoc/>
+        [Column("name")]
+        public string Name { get; set; }
+
+        /// <inheritdoc/>
+        [Column("quantity")]
+        public int Quantity { get; set; }
+
+        /// <inheritdoc/>
+        [Column("price")]
+        public decimal Price { get; set; }
+
+        /// <inheritdoc/>
+        [Column("exported")]
+        public bool Exported { get; set; }
+    }
+}
