b1ab02a606e9e79be9dd33bf41384875b2152d83
rs@merchello.com <rs@merchello.com>
2016-09-14 13:01:05 -0700

    Moves base mapper code.

--- src/Merchello.Core/Persistence/Mappers/BaseMapper.cs
+++ src/Merchello.Core/Persistence/Mappers/BaseMapper.cs
@@ -6,21 +6,37 @@
     using System.Reflection;
 
     using Merchello.Core.Acquired;
+    using Merchello.Core.Acquired.Persistence.SqlSyntax;
 
     using NPoco;
 
+    /// <summary>
+    /// Represents a the base mapper used to help build queries in SQL specific syntaxes.
+    /// </summary>
+    /// <remarks>
+    /// This is essentially the same class as Umbraco's class with the same name only the reference types are Merchello's
+    /// for Merchello specific mappings.
+    /// </remarks>
+    /// <seealso cref="https://github.com/umbraco/Umbraco-CMS/blob/dev-v8/src/Umbraco.Core/Persistence/Mappers/BaseMapper.cs"/>
     internal abstract class BaseMapper
     {
-
+        /// <summary>
+        /// Gets the thread safe dictionary of property info used.
+        /// </summary>
         internal abstract ConcurrentDictionary<string, DtoMapModel> PropertyInfoCache { get; }
 
 
-        internal string Map(string propertyName)
+        internal string Map(ISqlSyntaxProvider sqlSyntax, string propertyName, bool throws = false)
         {
             DtoMapModel dtoTypeProperty;
-            return PropertyInfoCache.TryGetValue(propertyName, out dtoTypeProperty)
-                ? GetColumnName(dtoTypeProperty.Type, dtoTypeProperty.PropertyInfo)
-                : string.Empty;
+
+            if (PropertyInfoCache.TryGetValue(propertyName, out dtoTypeProperty))
+                return GetColumnName(sqlSyntax, dtoTypeProperty.Type, dtoTypeProperty.PropertyInfo);
+
+            if (throws)
+                throw new InvalidOperationException("Could not get the value with the key " + propertyName + " from the property info cache, keys available: " + string.Join(", ", PropertyInfoCache.Keys));
+
+            return string.Empty;
         }
 
         internal void CacheMap<TSource, TDestination>(Expression<Func<TSource, object>> sourceMember, Expression<Func<TDestination, object>> destinationMember)
@@ -37,20 +53,16 @@
             return new DtoMapModel(typeof(TDestination), destination, source.Name);
         }
 
-        internal virtual string GetColumnName(Type dtoType, PropertyInfo dtoProperty)
+        internal virtual string GetColumnName(ISqlSyntaxProvider sqlSyntax, Type dtoType, PropertyInfo dtoProperty)
         {
             var tableNameAttribute = dtoType.FirstAttribute<TableNameAttribute>();
             string tableName = tableNameAttribute.Value;
 
             var columnAttribute = dtoProperty.FirstAttribute<ColumnAttribute>();
-            string columnName = columnAttribute.Name;
+            var columnName = columnAttribute.Name;
 
-            // TODO
-            //string columnMap = string.Format("{0}.{1}",
-            //                                 SqlSyntaxContext.SqlSyntaxProvider.GetQuotedTableName(tableName),
-            //                                 SqlSyntaxContext.SqlSyntaxProvider.GetQuotedColumnName(columnName));
-            //return columnMap;
-            return string.Empty;
+            var columnMap = sqlSyntax.GetQuotedTableName(tableName) + "." + sqlSyntax.GetQuotedColumnName(columnName);
+            return columnMap;
         }
     }
 }
--- src/Merchello.Core/Persistence/Mappers/DtoMapModel.cs
+++ src/Merchello.Core/Persistence/Mappers/DtoMapModel.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Reflection;
-
-namespace Merchello.Core.Persistence.Mappers
+﻿namespace Merchello.Core.Persistence.Mappers
 {
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Represents the result of a model mapped to a DTO (database POCO)
+    /// </summary>
+    /// <seealso cref="https://github.com/umbraco/Umbraco-CMS/blob/dev-v8/src/Umbraco.Core/Persistence/Mappers/DtoMapModel.cs"/>
     internal class DtoMapModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DtoMapModel"/> class.
+        /// </summary>
+        /// <param name="type">
+        /// The type of the model.
+        /// </param>
+        /// <param name="propertyInfo">
+        /// The property info.
+        /// </param>
+        /// <param name="sourcePropertyName">
+        /// The source property name.
+        /// </param>
         public DtoMapModel(Type type, PropertyInfo propertyInfo, string sourcePropertyName)
         {
             Type = type;
@@ -12,8 +28,19 @@ namespace Merchello.Core.Persistence.Mappers
             SourcePropertyName = sourcePropertyName;
         }
 
+        /// <summary>
+        /// Gets or sets the source property name.
+        /// </summary>
         public string SourcePropertyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
         public Type Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property info.
+        /// </summary>
         public PropertyInfo PropertyInfo { get; set; }
     }
 }
