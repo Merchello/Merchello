2424a590f38738e74c8ce87f08ace3546a828000
rs@merchello.com <rs@merchello.com>
2016-09-28 09:59:04 -0700

    Introduces IDeepCloneable

---
+++ src/Merchello.Core/Models/IDeepCloneable.cs
@@ -0,0 +1,16 @@
+﻿namespace Merchello.Core.Models
+{
+    /// <summary>
+    /// Represents an entity that can be deep cloned.
+    /// </summary>
+    public interface IDeepCloneable
+    {
+        /// <summary>
+        /// Deep clones the entity.
+        /// </summary>
+        /// <returns>
+        /// The cloned <see cref="object"/>.
+        /// </returns>
+        object DeepClone();
+    }
+}
