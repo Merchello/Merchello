7d149ead5c5ea445939742ab70d3836423da1fce
rs@merchello.com <rs@merchello.com>
2017-02-21 09:31:34 -0800

    Housekeeping

--- src/Merchello.FastTrack.Ui/App_Code/CustomerCxtDataViewer.cs
+++ src/Merchello.FastTrack.Ui/App_Code/CustomerCxtDataViewer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Merchello.FastTrack.Tests
+﻿namespace Merchello.FastTrack.Tests
 {
     using Merchello.Core.Models;
     using Merchello.Web;
