316992db067084c262c42cf469c28bd96cee0502
rs@merchello.com <rs@merchello.com>
2017-03-16 16:26:20 -0700

    Wires embed view for reports viewer.

--- src/Merchello.Web.UI.Client/src/views/reports/reports.viewreport.controller.js
+++ src/Merchello.Web.UI.Client/src/views/reports/reports.viewreport.controller.js
@@ -12,34 +12,40 @@
         ['$scope', '$routeParams', 'settingsResource',
          function($scope, $routeParams, settingsResource) {
 
-             $scope.loaded = true;
-             $scope.preValuesLoaded = true;
+             $scope.loaded = false;
+
+             $scope.reportPath = '';
 
              // Property to control the report to show
-             $scope.reportParam = $routeParams.id;
+             var reportParam  = $routeParams.id;
 
              settingsResource.getReportBackofficeTrees().then(function(trees) {
 
                  if(trees.length > 0) {
                      console.info(trees);
-                     var tree = _.find(trees, function(t) {
-
+                     var tree = _.find(trees, function (t) {
+                         if (t.routeId == reportParam) {
+                             return t;
+                         }
                      });
 
-                 } else {
+                     if (tree !== undefined) {
+                        $scope.reportPath = tree.routePath;
+                     }
+                 }
+
+                 if($scope.routePath === '') {
                      var re = /(\\)/g;
                      var subst = '/';
 
-                     var result = $scope.reportParam.replace(re, subst);
+                     var result = reportParam.replace(re, subst);
 
                      //$scope.reportPath = "/App_Plugins/Merchello.ExportOrders|ExportOrders.html";
                      $scope.reportPath = "/App_Plugins/" + result + ".html";
                  }
+
+                 $scope.loaded = true;
              });
-
-
-
-
 
 
 
