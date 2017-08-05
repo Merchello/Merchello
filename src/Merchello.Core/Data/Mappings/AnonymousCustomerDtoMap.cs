ab94b849949c225ff72ac1a248faa2dce6a63078
rs@merchello.com <rs@merchello.com>
2017-08-05 08:43:33 -0700

    [WIP] Move model builder entity configurations to separate classes.

---
+++ src/Merchello.Core/Data/Mappings/AnonymousCustomerDtoMap.cs
@@ -0,0 +1,41 @@
+﻿namespace Merchello.Core.Data.Mappings
+{
+    using Merchello.Core.Data.Models;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    internal class AnonymousCustomerDtoMap : DbEntityConfiguration<AnonymousCustomerDto>
+    {
+        public override void Configure(EntityTypeBuilder<AnonymousCustomerDto> entity)
+        {
+            entity.HasKey(e => e.Pk)
+                .HasName("PK_merchAnonymousCustomer");
+
+            entity.ToTable("merchAnonymousCustomer");
+
+            entity.Property(e => e.Pk)
+                .HasColumnName("pk")
+                .HasDefaultValueSql("'newid()'");
+
+            entity.Property(e => e.CreateDate)
+                .HasColumnName("createDate")
+                .HasColumnType("datetime")
+                .HasDefaultValueSql("getdate()");
+
+            entity.Property(e => e.ExtendedData)
+                .HasColumnName("extendedData")
+                .HasColumnType("ntext");
+
+            entity.Property(e => e.LastActivityDate)
+                .HasColumnName("lastActivityDate")
+                .HasColumnType("datetime")
+                .HasDefaultValueSql("getdate()");
+
+            entity.Property(e => e.UpdateDate)
+                .HasColumnName("updateDate")
+                .HasColumnType("datetime")
+                .HasDefaultValueSql("getdate()");
+        }
+    }
+}
---
+++ src/Merchello.Core/Data/Mappings/AppliedPaymentDtoMap.cs
@@ -0,0 +1,62 @@
+﻿namespace Merchello.Core.Data.Mappings
+{
+    using Merchello.Core.Data.Models;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    internal class AppliedPaymentDtoMap : DbEntityConfiguration<AppliedPaymentDto>
+    {
+        public override void Configure(EntityTypeBuilder<AppliedPaymentDto> entity)
+        {
+                entity.HasKey(e => e.Pk)
+                    .HasName("PK_merchAppliedPayment");
+
+                entity.ToTable("merchAppliedPayment");
+
+                entity.Property(e => e.Pk)
+                    .HasColumnName("pk")
+                    .HasDefaultValueSql("'newid()'");
+
+                entity.Property(e => e.Amount)
+                    .HasColumnName("amount")
+                    .HasColumnType("numeric");
+
+                entity.Property(e => e.AppliedPaymentTfKey).HasColumnName("appliedPaymentTfKey");
+
+                entity.Property(e => e.CreateDate)
+                    .HasColumnName("createDate")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("getdate()");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasColumnName("description")
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Exported).HasColumnName("exported");
+
+                entity.Property(e => e.InvoiceKey).HasColumnName("invoiceKey");
+
+                entity.Property(e => e.PaymentKey).HasColumnName("paymentKey");
+
+                entity.Property(e => e.UpdateDate)
+                    .HasColumnName("updateDate")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("getdate()");
+
+                entity.HasOne(d => d.InvoiceKeyNavigation)
+                    .WithMany(p => p.MerchAppliedPayment)
+                    .HasForeignKey(d => d.InvoiceKey)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_merchAppliedPayment_merchInvoice");
+
+                entity.HasOne(d => d.PaymentKeyNavigation)
+                    .WithMany(p => p.MerchAppliedPayment)
+                    .HasForeignKey(d => d.PaymentKey)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_merchAppliedPayment_merchPayment");
+        }
+    }
+}
