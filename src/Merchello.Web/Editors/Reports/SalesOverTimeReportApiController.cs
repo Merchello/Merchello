8e34c182883494b47e85de514058b5501ce63c64
Sam Gooch <sam@samgooch.co.uk>
2017-10-31 12:53:51 +0000

    Remove test code

--- src/Merchello.Web/Editors/Reports/SalesOverTimeReportApiController.cs
+++ src/Merchello.Web/Editors/Reports/SalesOverTimeReportApiController.cs
@@ -170,7 +170,6 @@
             }
 
             var weekStarting = weekEnding.StartOfWeek();
-            weekStarting = weekStarting.AddDays(-56);
             weekEnding = weekStarting.AddDays(6);
 
 
