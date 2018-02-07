36d6702fbc21ad44de237aed32504cbcd37c685c
Lee Messenger <lee@aptitude.co.uk>
2018-02-07 07:36:33 +0000

    Fixing NPM dependencies

---
+++ src/Merchello.Providers.UI.Client/build/App_Plugins/MerchelloProviders/js/merchello.providers.resources.js
@@ -0,0 +1,39 @@
+/*! MerchelloPaymentProviders
+ * https://github.com/Merchello/Merchello
+ * Copyright (c) 2018 Across the Pond, LLC.
+ * Licensed MIT
+ */
+
+(function() { 
+
+angular.module('merchello.providers.resources').factory('braintreeResource',
+    ['$http', 'umbRequestHelper',
+        function($http, umbRequestHelper) {
+
+            var baseUrl = Umbraco.Sys.ServerVariables["merchelloPaymentsUrls"]["merchelloBraintreeApiBaseUrl"];
+
+            return {
+
+                getClientRequestToken: function (customerKey) {
+                    if (customerKey != null & customerKey != undefined) {
+                        return umbRequestHelper.resourcePromise(
+                            $http({
+                                url: baseUrl + 'GetClientRequestToken',
+                                method: "GET",
+                                params: { customerKey: customerKey }
+                            }),
+                            'Failed to retreive customer request token');
+                    } else {
+                        return umbRequestHelper.resourcePromise(
+                            $http({
+                                url: baseUrl + 'GetClientRequestToken',
+                                method: "GET"
+                            }),
+                            'Failed to retreive customer request token');
+                    }
+                }
+            };
+        }]);
+
+
+})();
---
+++ src/Merchello.Providers.UI.Client/build/App_Plugins/MerchelloProviders/js/merchello.providers.services.js
@@ -0,0 +1,11 @@
+/*! MerchelloPaymentProviders
+ * https://github.com/Merchello/Merchello
+ * Copyright (c) 2018 Across the Pond, LLC.
+ * Licensed MIT
+ */
+
+(function() { 
+
+
+
+})();
