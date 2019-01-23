b752c831e767e598d08e91b43bf4d540fd672722
Sam Gooch <sam@samgooch.co.uk>
2019-01-23 10:34:50 +0000

    Add sellPrice sort so that a user can sort by the price it will sell at

--- src/Merchello.Web/QueryBuilderEnumerations.cs
+++ src/Merchello.Web/QueryBuilderEnumerations.cs
@@ -44,6 +44,11 @@
         /// <summary>
         /// The product SKU 
         /// </summary>
-        Sku
+        Sku,
+
+        /// <summary>
+        /// The product sell price (sale price if present) 
+        /// </summary>
+        SellPrice
     }
 }
