324165d1f4fdc68b183b685ac7e85e0ab245f565
Sam Gooch <sam@samgooch.co.uk>
2019-02-07 13:13:55 +0000

    Add predetermined date ranges to date selection picker

--- src/Merchello.FastTrack.Ui/App_Plugins/Merchello/js/merchello.controllers.js
+++ src/Merchello.FastTrack.Ui/App_Plugins/Merchello/js/merchello.controllers.js
@@ -775,6 +775,9 @@ angular.module('merchello').controller('Merchello.Common.Dialogs.DateRangeSelect
         $scope.preValuesLoaded = false;
 
         $scope.changeDateFilters = changeDateFilters;
+        $scope.preSelectDate = preSelectDate;
+        $scope.preSelectDays = preSelectDays;
+        $scope.preSelectMonths = preSelectMonths;
 
         $scope.dateFormat = 'YYYY-MM-DD';
         $scope.rangeStart = '';
@@ -874,6 +877,32 @@ angular.module('merchello').controller('Merchello.Common.Dialogs.DateRangeSelect
         function changeDateFilters(start, end) {
             $scope.rangeStart = start;
             $scope.rangeEnd = end;
+        }
+
+        function preSelectDays(days) {
+            var end = new Date();
+            var start = new Date().setDate(new Date().getDate() - days);
+            preSelectDate(moment(start).format($scope.dateFormat), moment(end).format($scope.dateFormat));
+        }
+
+        function preSelectMonths(months) {
+            var end = new Date();
+            var start = new Date().setMonth(new Date().getMonth() - months);
+            preSelectDate(moment(start).format($scope.dateFormat), moment(end).format($scope.dateFormat));
+        }
+
+        /**
+         * @ngdoc method
+         * @name preSelectDate
+         * @function
+         *
+         * @param {string} start - String representation of start date.
+         * @param {string} end - String representation of end date.
+         * @description - Change the date filters, then triggera new API call to load the reports.
+         */
+        function preSelectDate(start, end) {
+            changeDateFilters(start, end);
+            save();
         }
 
         /*-------------------------------------------------------------------
